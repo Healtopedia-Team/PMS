5923e37b1f59690581f253e49914bb397f7f0dc3
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-12 15:38:42 +0800

    Add files via upload

---
+++ view-appointment.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appointment #2101 Details</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                        </div>
+                        <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Menu</li>
+
+                        <li class="sidebar-item  ">
+                            <a href="index.html" class='sidebar-link'>
+                                <i class="bi bi-grid-fill"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item active ">
+                            <a href="form-layout.html" class='sidebar-link'>
+                                <i class="bi bi-file-earmark-medical-fill"></i>
+                                <span>Add Appointment</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            <div class="page-heading">
+                <div class="page-title">
+                    <div class="row">
+                        <div class="col-12 col-md-6 order-md-1 order-last">
+                            <h3>Appointment #2101 Details</h3>
+                            <p class="text-subtitle text-muted">Appointment Made at 12/2/2021 at 8:50 a.m | Status:
+                                <span class="badge bg-primary">Booked</span>
+                            </p>
+                        </div>
+                        <div class="col-12 col-md-6 order-md-2 order-first">
+                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Form Layout</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+
+                <style>
+                    div.quest {
+                        float: left;
+                        width: 30%;
+                        font-weight: bold;
+                    }
+
+                    div.ans {
+                        float: right;
+                        width: 70%;
+
+                    }
+                </style>
+
+                <!-- // Basic multiple Column Form section start -->
+                <section id="multiple-column-form">
+                    <div class="row match-height">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4>Personal Details</h4>
+                                    <hr>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-body" style="margin-top: -40px;">
+                                        <div class="quest">Name: </div>
+                                        <div class="ans">Siti Hajar binti Mohd Rafie</div>
+                                        <div class="quest">Phone No:</div>
+                                        <div class="ans">0137390349</div>
+                                        <div class="quest">IC/Passport :</div>
+                                        <div class="ans">980906085394</div>
+                                        <div class="quest">Nationality :</div>
+                                        <div class="ans">Malaysian</div>
+                                        <div class="quest">Gender :</div>
+                                        <div class="ans">Female</div>
+                                        <div class="quest">Birth Date :</div>
+                                        <div class="ans">16011998</div>
+                                        <div class="quest">Email :</div>
+                                        <div class="ans">hajarrafie98@gmail.com</div>
+                                        <div class="quest">Billing Address :</div>
+                                        <div class="ans">NO.17 LORONG CAHAYA 7 TAMAN CAHAYA,45300, SUNGAI BESAR <br><br>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Appointment(s) Details</h4>
+                                <hr>
+                            </div>
+                            <div class="card-content">
+                                <div class="card-body" style="margin-top: -40px;">
+                                    <div>
+                                        <h5><span class="badge bg-light-primary">Walk-In/Drive-Thru Appointment – RT-PCR
+                                                Covid-19
+                                                Test [KPJ Bandar Dato' Onn]</span><br></h5>
+                                    </div>
+
+                                    <div class="quest">Appointment Date:</div>
+                                    <div class="ans">14-04-2021</div>
+                                    <div class="quest">Appointment Time :</div>
+                                    <div class="ans">05:00 PM</div>
+                                    <div class="quest">Package Fee :</div>
+                                    <div class="ans">RM 210.00</div>
+                                    <div class="quest">Payment via :</div>
+                                    <div class="ans">Credit Card</div>
+                                    <div style="height: 120px;">
+                                        <hr>
+                                    </div>
+
+
+                                    <ul class="list-group">
+                                        <li class="list-group-item">
+                                            <div class="quest">Attendee Name :</div>
+                                            <div class="ans">Mohd Rafie bin Mohd Amin</div>
+                                            <div class="quest">IC/ Passport :</div>
+                                            <div class="ans">690719105833</div>
+                                            <div class="quest">Nationality :</div>
+                                            <div class="ans">Malaysian</div>
+                                            <div class="quest">Gender :</div>
+                                            <div class="ans">Male</div>
+                                            <div class="quest">Birth Date :</div>
+                                            <div class="ans">19071969</div>
+                                        </li>
+                                    </ul>
+                                    <hr>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-content">
+                                        <div class="card-body" style="margin-top: -40px;">
+                                            <div>
+                                                <h5><span class="badge bg-light-primary">Wellness Woman [KPJ Bandar Dato' Onn]</span><br></h5>
+                                            </div>
+        
+                                            <div class="quest">Appointment Date:</div>
+                                            <div class="ans">14-04-2021</div>
+                                            <div class="quest">Appointment Time :</div>
+                                            <div class="ans">05:00 PM</div>
+                                            <div class="quest">Package Fee :</div>
+                                            <div class="ans">RM 210.00</div>
+                                            <div class="quest">Payment via :</div>
+                                            <div class="ans">Credit Card</div>
+                                            <div style="height: 120px;">
+                                                <hr>
+                                            </div>
+        
+        
+                                            <ul class="list-group">
+                                                <li class="list-group-item">
+                                                    <div class="quest">Attendee Name :</div>
+                                                    <div class="ans">Mohd Rafie bin Mohd Amin</div>
+                                                    <div class="quest">IC/ Passport :</div>
+                                                    <div class="ans">690719105833</div>
+                                                    <div class="quest">Nationality :</div>
+                                                    <div class="ans">Malaysian</div>
+                                                    <div class="quest">Gender :</div>
+                                                    <div class="ans">Male</div>
+                                                    <div class="quest">Birth Date :</div>
+                                                    <div class="ans">19071969</div>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <div class="quest">Attendee Name :</div>
+                                                    <div class="ans">Mohd Rafie bin Mohd Amin</div>
+                                                    <div class="quest">IC/ Passport :</div>
+                                                    <div class="ans">690719105833</div>
+                                                    <div class="quest">Nationality :</div>
+                                                    <div class="ans">Malaysian</div>
+                                                    <div class="quest">Gender :</div>
+                                                    <div class="ans">Male</div>
+                                                    <div class="quest">Birth Date :</div>
+                                                    <div class="ans">19071969</div>
+                                                </li>
+                                            </ul>
+                                            <hr>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            </section>
+            <!-- // Basic multiple Column Form section end -->
+        </div>
+
+        <footer>
+            <div class="footer clearfix mb-0 text-muted">
+                <div class="float-start">
+                    <p>2021 &copy; Mazer</p>
+                </div>
+                <div class="float-end">
+                    <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                            href="http://ahmadsaugi.com">A. Saugi</a></p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    </div>
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
