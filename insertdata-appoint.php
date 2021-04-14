a6f4d97c75c16b30be9986dbd472443b5ebdb51d
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-14 12:56:54 +0800

    Add files via upload

---
+++ insertdata-appoint.php
@@ -0,0 +1,26 @@
+<?php
+    
+    header("refresh: 60");
+
+    $data = file_get_contents('http://app-pms.eopm4g7bxo-jqp3vpjlj350.p.runcloud.link/slotjson.php');
+    $data = json_decode($data, true);
+
+    $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
+
+    foreach ($data as $row){
+        $orderid = $row['order_id'];
+        $appointid = $row['id'];
+        $startappoint = $row['start'];
+        $endappoint = $row['end'];
+  
+        $result=mysqli_query($conn, "SELECT COUNT(appoint_id) as Total FROM appointwoo WHERE appoint_id = '$appointid'");
+        $user=mysqli_fetch_all($result, MYSQLI_ASSOC);
+  
+        foreach ($user as $key) {
+            if ($key['Total'] < 1) {
+                $sql = "INSERT INTO appointwoo SET order_id = '$orderid', appoint_id = '$appointid', start_appoint = '$startappoint', end_appoint = '$endappoint'";
+                mysqli_query($conn, $sql);
+            }
+        }
+    }
+?>
