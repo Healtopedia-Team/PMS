0aa97b95739016ee033ef45b41a441210980a537
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-14 13:30:10 +0800

    Update insertdata-appoint.php

--- insertdata-appoint.php
+++ insertdata-appoint.php
@@ -10,15 +10,15 @@
     foreach ($data as $row){
         $orderid = $row['order_id'];
         $appointid = $row['id'];
-        $startappoint = $row['start'];
-        $endappoint = $row['end'];
+        $dateappoint = date("d-m-Y", $data['start']-28800);
+        $startappoint = date("H:i", $data['start']-28800);
   
         $result=mysqli_query($conn, "SELECT COUNT(appoint_id) as Total FROM appointwoo WHERE appoint_id = '$appointid'");
         $user=mysqli_fetch_all($result, MYSQLI_ASSOC);
   
         foreach ($user as $key) {
             if ($key['Total'] < 1) {
-                //$sql = "INSERT INTO appointwoo SET order_id = '$orderid', appoint_id = '$appointid', start_appoint = '$startappoint', end_appoint = '$endappoint'";
+                $sql = "INSERT INTO appointwoo SET order_id = '$orderid', appoint_id = '$appointid', date_appoint = '$dateappoint', start_appoint = '$startappoint'";
                 mysqli_query($conn, $sql);
             }
         }
