06bca9c6289cc34d17d624b7f0f94cf42b7e38e6
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-15 13:52:08 +0800

    Update test.php

--- test.php
+++ test.php
@@ -7,4 +7,4 @@ echo strtotime('-8 hour'); //timestamp 24 hours from now
 echo date('h:i A', $times);
 
 $time_given = 1618563600;
-echo date('h:i A',strtotime('-8 hours',strtotime($time_given)));
+echo date('h:i A', $time_given);
