43675eb5bd32f0e00257750c7e63a643513e4700
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-15 12:19:17 +0800

    Update test.php

--- test.php
+++ test.php
@@ -4,8 +4,11 @@ echo strtotime('+7 hour'); //current timestamp
 echo strtotime('+32 hour'); //timestamp 24 hours from now
 
 $timestamp = strtotime('today midnight');
+$timestamp = strtotime('+8 hour');
 echo $timestamp;
 
 $timestamp2 = strtotime('tomorrow midnight');
+$timestamp2 = strtotime('+8 hour');
+
 echo $timestamp2;
 
