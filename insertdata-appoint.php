4098e282db4aaa3d2b90587705694e7c9d00de8e
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-16 15:26:34 +0800

    Update insertdata-appoint.php

--- insertdata-appoint.php
+++ insertdata-appoint.php
@@ -2,10 +2,10 @@
     
     header("refresh: 60");
 
-    $data = file_get_contents('http://app-pms.eopm4g7bxo-jqp3vpjlj350.p.runcloud.link/slotjson.php');
+    $data = file_get_contents('https://pms.healtopedia.com/slotjson.php');
     $data = json_decode($data, true);
     
-    $data2 = file_get_contents('http://app-pms.eopm4g7bxo-jqp3vpjlj350.p.runcloud.link/productjson.php');
+    $data2 = file_get_contents('https://pms.healtopedia.com/productjson.php');
     $data2 = json_decode($data2, true);
 
     $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
