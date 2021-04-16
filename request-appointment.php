d8b250634b2c98f3a22e15ae1db77245643ba7fb
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-16 11:22:52 +0800

    Update request-appointment.php

--- request-appointment.php
+++ request-appointment.php
@@ -101,7 +101,7 @@
                                         <button type="button" class="btn btn-outline-primary">Upcoming</button>
                                         <button type="button" class="btn btn-outline-primary">Pending</button>
 				</div>
-				<button type="button" class="btn btn-success mb-3" style="position: absolute;right: 0;" data-bs-toggle="modal" data-bs-target="#exampleModalScrollable">
+				<button type="button" class="btn btn-success mb-3" style="position: absolute;right: 3;" data-bs-toggle="modal" data-bs-target="#exampleModalScrollable">
 					Add Request
 				</button>
 				<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog"
