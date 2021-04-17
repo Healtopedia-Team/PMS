a42dc1b1ba0e7d0aac1581b7bbd4a86676bfb1f1
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-17 08:08:37 +0800

    Update request-appointment.php

--- request-appointment.php
+++ request-appointment.php
@@ -167,20 +167,11 @@ $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
                                                 <select class="form-select" id="basicSelect" name="reqstatus">
                                                     <option value="pending">Pending</option>
                                                     <option value="complete">Complete</option>
-                                                </select>
+                                                </select><br>
+                                                
+                                                <button type="submit" name="submitrequest" class="btn btn-primary ml-1" style="position: relative;float: right;z-index: 597;">Submit</button><br><br>
                                         </div>
                                         </form>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
-                                                <i class="bx bx-x d-block d-sm-none"></i>
-                                                <span class="d-none d-sm-block">Close</span>
-                                            </button>
-
-                                            <button type="button" class="btn btn-primary ml-1" data-bs-dismiss="modal">
-                                                <i class="bx bx-check d-block d-sm-none"></i>
-                                                <span class="d-none d-sm-block">Accept</span>
-                                            </button>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
