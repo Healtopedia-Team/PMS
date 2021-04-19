9668e837c67cfdd76cb1cbf5a97d18b244f568d2
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-19 13:43:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ $appointment = mysqli_fetch_all($result, MYSQLI_ASSOC);
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
                                 <li>
-                                    <h6 class="dropdown-header">Hello, John!</h6>
+                                    <h6 class="dropdown-header">Hello, <?php echo $_SESSION["name"]?>!</h6>
                                 </li>
                                 <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
                                         Profile</a></li>
