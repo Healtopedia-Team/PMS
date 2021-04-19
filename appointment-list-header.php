a1a50a947c3745fc40d5c35564b51eb90d1945e1
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-19 12:29:03 +0800

    Update appointment-list-header.php

--- appointment-list-header.php
+++ appointment-list-header.php
@@ -20,49 +20,7 @@
 
 <body>
     <div id="app">
-        <div id="sidebar" class="active">
-            <div class="sidebar-wrapper active">
-                <div class="sidebar-header">
-                    <div class="d-flex justify-content-between">
-                        <div class="logo">
-                            <a href="index.php"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
-                        </div>
-                        <div class="toggler">
-                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="sidebar-menu">
-                    <ul class="menu">
-
-                        <li class="sidebar-title">Forms &amp; Tables</li>
-                      
-                      	<li class="sidebar-item">
-                            <a href="request-appointment.php" class='sidebar-link'>
-                                <i class="bi bi-person-check-fill"></i>
-                                <span>Request Appointment</span>
-                            </a>
-                        </li>
-			    
-			<li class="sidebar-item active">
-                            <a href="appointment-list.php" class='sidebar-link'>
-                                <i class="bi bi-list-ul"></i>
-                                <span>Appointment List</span>
-                            </a>
-                        </li>
-			    
-			<li class="sidebar-item">
-                            <a href="setting.php" class='sidebar-link'>
-                                <i class="bi bi-gear-fill"></i>
-                                <span>Setting</span>
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
-            </div>
-        </div>
+        <?php include 'sidebar.php'; ?>
         <div id="main">
             <header class="mb-3">
                 <a href="#" class="burger-btn d-block d-xl-none">
