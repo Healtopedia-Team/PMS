d3a98baf6379b6709998abb0d5eca3032ad3973f
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-20 16:04:42 +0800

    Update test.php

--- test.php
+++ test.php
@@ -56,7 +56,19 @@ $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
                 <section class="section">
                     <div class="card">
                         <div class="card-body">
-                            <form method="POST" name="formdate">
+                            <section id="basic-horizontal-layouts">
+                                <div class="row match-height">
+                                    <div class="col-md-6 col-12">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <h4 class="card-title">Check Date</h4>
+                                            </div>
+                                            <div class="card-content">
+                                                <div class="card-body">
+                                                    <form class="form form-horizontal">
+                                                        <div class="form-body">
+                                                            <div class="row">
+                                                                <form method="POST" name="formdate">
                                 <div>
                                     <input type="text" id="date" name="date" class="form-control datepicker" size="5" autocomplete="off" placeholder="click here.." value="<?= $appdate ?>" required>
                                     <br>
@@ -78,6 +90,103 @@ $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
                                     ?>
                                 </div>
                             </form>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-12">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <h4 class="card-title">Horizontal Form with Icons</h4>
+                                            </div>
+                                            <div class="card-content">
+                                                <div class="card-body">
+                                                    <form class="form form-horizontal">
+                                                        <div class="form-body">
+                                                            <div class="row">
+                                                                <div class="col-md-4">
+                                                                    <label>Name</label>
+                                                                </div>
+                                                                <div class="col-md-8">
+                                                                    <div class="form-group has-icon-left">
+                                                                        <div class="position-relative">
+                                                                            <input type="text" class="form-control"
+                                                                                placeholder="Name" id="first-name-icon">
+                                                                            <div class="form-control-icon">
+                                                                                <i class="bi bi-person"></i>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-md-4">
+                                                                    <label>Email</label>
+                                                                </div>
+                                                                <div class="col-md-8">
+                                                                    <div class="form-group has-icon-left">
+                                                                        <div class="position-relative">
+                                                                            <input type="email" class="form-control"
+                                                                                placeholder="Email" id="first-name-icon">
+                                                                            <div class="form-control-icon">
+                                                                                <i class="bi bi-envelope"></i>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-md-4">
+                                                                    <label>Mobile</label>
+                                                                </div>
+                                                                <div class="col-md-8">
+                                                                    <div class="form-group has-icon-left">
+                                                                        <div class="position-relative">
+                                                                            <input type="number" class="form-control"
+                                                                                placeholder="Mobile">
+                                                                            <div class="form-control-icon">
+                                                                                <i class="bi bi-phone"></i>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-md-4">
+                                                                    <label>Password</label>
+                                                                </div>
+                                                                <div class="col-md-8">
+                                                                    <div class="form-group has-icon-left">
+                                                                        <div class="position-relative">
+                                                                            <input type="password" class="form-control"
+                                                                                placeholder="Password">
+                                                                            <div class="form-control-icon">
+                                                                                <i class="bi bi-lock"></i>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group col-md-8 offset-md-4">
+                                                                    <div class='form-check'>
+                                                                        <div class="checkbox">
+                                                                            <input type="checkbox" id="checkbox2"
+                                                                                class='form-check-input' checked>
+                                                                            <label for="checkbox2">Remember Me</label>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-12 d-flex justify-content-end">
+                                                                    <button type="submit"
+                                                                        class="btn btn-primary me-1 mb-1">Submit</button>
+                                                                    <button type="reset"
+                                                                        class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
                         </div>
                     </div>
                 </section>
