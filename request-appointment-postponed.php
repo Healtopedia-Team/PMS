1d631159972128de79cdc47fa86206b4d8e1a9d5
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-20 13:51:20 +0800

    Update request-appointment-postponed.php

--- request-appointment-postponed.php
+++ request-appointment-postponed.php
@@ -14,7 +14,7 @@ $result = mysqli_query($conn,"SELECT * FROM requestappoint WHERE req_status = 'p
                                         <th>Status</th>
                                         <th>Action</th>
                                     </tr>
-                                </thead>>
+                                </thead>
                                     <tr>
                                         <td>Muaz</td>
                                         <td>Mental Health Screening</td>
