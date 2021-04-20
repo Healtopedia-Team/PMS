75dda537c791e0c4bd898cfe4041b1050d1a2138
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-20 11:28:54 +0800

    Add files via upload

---
+++ scripttime.js
@@ -0,0 +1,82 @@
+//====================================M=O=R=N=I=N=G===================================//
+function myTime() {
+  var x = document.getElementById("time").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime1() {
+  var x = document.getElementById("time1").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime2() {
+  var x = document.getElementById("time2").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+//====================================A=F=T=E=R=N=O=O=N=================================//
+
+function myTime3() {
+  var x = document.getElementById("time3").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime4() {
+  var x = document.getElementById("time4").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime5() {
+  var x = document.getElementById("time5").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime6() {
+  var x = document.getElementById("time6").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime7() {
+  var x = document.getElementById("time7").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime8() {
+  var x = document.getElementById("time8").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime9() {
+  var x = document.getElementById("time9").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+//====================================E=V=E=N=I=N=G===================================//
+
+function myTime10() {
+  var x = document.getElementById("time10").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime11() {
+  var x = document.getElementById("time11").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
+
+function myTime12() {
+  var x = document.getElementById("time12").value;
+  document.getElementById("apptime").value= x;
+  document.getElementById("demo").innerHTML= x;
+}
