dae942da7cc236099e909d0b075cedd4553db864
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-22 15:51:25 +0800

    Update req-addpatient.php

--- req-addpatient.php
+++ req-addpatient.php
@@ -192,7 +192,9 @@
                                                                             <input type="text" name="latestid" class="form-control" value="<?php echo $last_id ?>" style="display: none;">
 
                                                                             <select class="choices form-select" name="packname">
-                                                                                    <option value="">Select Package</option>
+                                                                                    <?php foreach($data as $key){ ?>
+                                                                                        <option value="<?php echo $key['package_name'];?>"><?php echo $key['package_name'];?></option>
+                                                                                    <?php } ?>
                                                                             </select>
 
                                                                             <input type="text" name="packname" class="form-control" placeholder="Package Name" required>
