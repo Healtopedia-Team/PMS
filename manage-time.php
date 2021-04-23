c578013f23c6a4b0290a8f65958bb39f80687c34
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-23 12:27:07 +0800

    Update manage-time.php

--- manage-time.php
+++ manage-time.php
@@ -130,11 +130,7 @@ $user1=mysqli_fetch_all($result1, MYSQLI_ASSOC);
                 </div>
                 <section id="basic-horizontal-layouts">
                     <div class="row match-height">
-                        <div class="col-md-6 col-12">
                             <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title">Horizontal Form</h4>
-                                </div>
                                 <div class="card-content">
                                     <div class="card-body">
                                         <form method="POST">
@@ -360,16 +356,11 @@ $user1=mysqli_fetch_all($result1, MYSQLI_ASSOC);
                                     </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
                 </section>
                 <section id="basic-horizontal-layouts">
                     <div class="row match-height">
-                        <div class="col-md-6 col-12">
                             <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title">Horizontal Form</h4>
-                                </div>
                                 <div class="card-content">
                                     <div class="card-body">
                                         <table class="table table-striped" id="table1">
@@ -401,7 +392,6 @@ $user1=mysqli_fetch_all($result1, MYSQLI_ASSOC);
                                     </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
                 </section>
                 <!--section class="section">
