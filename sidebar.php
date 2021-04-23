b87ef12ae2c12151980a53553e7845ed5cb8d3b3
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-23 12:07:15 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -24,12 +24,18 @@ $your_variable = basename($_SERVER['PHP_SELF'], ".php");
                         <i class="bi bi-person-check-fill"></i>
                         <span>Request Appoint</span>
                     </a>
-                    <ul class="submenu <?php if ($your_variable=="request-appointment-all" || $your_variable=="request-addappoint" || $your_variable=="request-appointment-pending" || $your_variable=="request-appointment-postponed" || $your_variable=="request-appointment-approved") {echo "active"; }else{echo"noactive";}?>">
+                    <ul class="submenu <?php if ($your_variable=="request-appointment-all" || $your_variable=="request-addappoint" || $your_variable=="request-appointment-pending" || $your_variable=="request-appointment-postponed" || $your_variable=="request-appointment-approved" || $your_variable=="manage-date" || $your_variable=="manage-time") {echo "active"; }else{echo"noactive";}?>">
                         <li class="submenu-item <?php if ($your_variable=="request-appointment-all" || $your_variable=="request-appointment-pending" || $your_variable=="request-appointment-postponed" || $your_variable=="request-appointment-approved") {echo "active"; }else{echo"noactive";}?>">
                             <a href="request-appointment-all.php">Request List</a>
                         </li>
                         <li class="submenu-item <?php if ($your_variable=="request-addappoint") {echo "active"; }else{echo"noactive";}?>">
                             <a href="request-addappoint.php">Add Request</a>
+                        </li>
+                        <li class="submenu-item <?php if ($your_variable=="manage-date") {echo "active"; }else{echo"noactive";}?>">
+                            <a href="manage-date.php">Manage Date</a>
+                        </li>
+                        <li class="submenu-item <?php if ($your_variable=="manage-time") {echo "active"; }else{echo"noactive";}?>">
+                            <a href="manage-time.php">Manage Time</a>
                         </li>
                     </ul>
                 </li>
@@ -44,12 +50,9 @@ $your_variable = basename($_SERVER['PHP_SELF'], ".php");
                         <i class="bi bi-gear-fill"></i>
                         <span>Setting</span>
                     </a>
-                    <ul class="submenu <?php if ($your_variable=="manage-date" || $your_variable=="manage-time") {echo "active"; }else{echo"noactive";}?>">
-                        <li class="submenu-item <?php if ($your_variable=="manage-date") {echo "active"; }else{echo"noactive";}?>">
-                            <a href="manage-date.php">Manage Date</a>
-                        </li>
-                        <li class="submenu-item <?php if ($your_variable=="manage-time") {echo "active"; }else{echo"noactive";}?>">
-                            <a href="manage-time.php">Manage Time</a>
+                    <ul class="submenu <?php if ($your_variable=="update-profile") {echo "active"; }else{echo"noactive";}?>">
+                        <li class="submenu-item <?php if ($your_variable=="update-profile") {echo "active"; }else{echo"noactive";}?>">
+                            <a href="update-profile.php">Update Profile</a>
                         </li>
                     </ul>
                 </li>
