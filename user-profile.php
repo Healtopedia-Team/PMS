246781f5fbc76353c4f4ac6a3e36ce246e48a49d
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-26 17:16:07 +0800

    Update user-profile.php

--- user-profile.php
+++ user-profile.php
@@ -59,7 +59,7 @@ if(!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true){
                     <div class="card">
                         <div class="card-body">
                             <div class="avatar avatar-xxxl">
-                                <button onclick="functiontry()" style="border-radius: 50%;"><img src="assets/images/faces/1.jpg"></button>
+                                <img src="assets/images/faces/1.jpg" onclick="functiontry()">
                                 <script>
                                     function functiontry(){
                                         alert('DONE!!!!');
