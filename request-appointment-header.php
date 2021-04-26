3f8ec9ca1c5d5fb304e610cbe5912172d5e74c69
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-26 11:25:44 +0800

    Update request-appointment-header.php

--- request-appointment-header.php
+++ request-appointment-header.php
@@ -111,7 +111,7 @@ if(!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true){
                 </div>
             </nav>        
         </header>
-        <div id="main">
+        <div id="main" style="margin-top: -50px;">
             <div class="page-heading">
                 <div class="page-title">
                     <div class="row">
