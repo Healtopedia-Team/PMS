ec5d6485af7f99f8dc50722113d3e358d3784d28
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-26 15:46:39 +0800

    Update user-profile.php

--- user-profile.php
+++ user-profile.php
@@ -51,7 +51,7 @@ if(!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true){
                 <section class="section">
                     <div class="card">
                         <div class="card-body">
-                            <div class="avatar avatar-xxxl" style="text-align: center;">
+                            <div class="avatar avatar-xxxl">
                                 <img src="assets/images/faces/1.jpg">
                             </div>
                             <div class="avatar avatar-xxxl" style="text-align: center;">
