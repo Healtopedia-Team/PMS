4dd8d9ddcfccd662f833e0908d09ff1c1a0978cc
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-26 17:47:40 +0800

    Update user-profile.php

--- user-profile.php
+++ user-profile.php
@@ -58,25 +58,24 @@ if(!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true){
                 <section class="section">
                     <div class="card">
                         <div class="card-body">
-                            <!--div class="avatar avatar-xxxl">
+                            <div class="avatar avatar-xxxl">
                                 <button type="button" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-placement="left" title="click to change" onclick="functiontry()" style="border-radius: 50%;">
                                         <img src="assets/images/faces/1.jpg">
                                 </button>
-                            </div-->
-                            <div>
+                                <br>
                                 <center><br>
                                     <?php foreach ($data as $row){ ?>
                                     <h6>Firstname</h6>
-                                    <input align="ce" type="text" name="firstnameuser" value="<?php echo $row['first_name'];?>" class="form-control">
+                                    <input align="ce" type="text" name="firstnameuser" value="<?php echo $row['first_name'];?>" class="form-control" style="width: 400px;">
                                     <br>
                                     <h6>Lastname</h6>
-                                    <input type="text" name="lastnameuser" value="<?php echo $row['last_name'];?>" class="form-control">
+                                    <input type="text" name="lastnameuser" value="<?php echo $row['last_name'];?>" class="form-control" style="width: 400px;">
                                     <br>
                                     <h6>Email</h6>
-                                    <input type="text" name="emailuser" value="<?php echo $row['email'];?>" >
+                                    <input type="text" name="emailuser" value="<?php echo $row['email'];?>" class="form-control" style="width: 400px;">
                                     <br>
                                     <h6>Hospital</h6>
-                                    <input type="text" name="hospitaluser" value="<?php echo $row['hospital'];?>" class="form-control">
+                                    <input type="text" name="hospitaluser" value="<?php echo $row['hospital'];?>" class="form-control" style="width: 400px;">
                                     <?php } ?>
                                     <br>
                                     <button type="submit" name="saveprofile" class="btn btn-primary">SAVE</button>
