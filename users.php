1364d97a7652636d24a0770d182e2455458df12c
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-26 10:53:43 +0800

    Update users.php

--- users.php
+++ users.php
@@ -29,7 +29,7 @@ $user = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
 <body>
     <div id="app">
-<?php include 'sidebar.php' ?>
+        <?php include 'sidebar.php' ?>
         <div id="main">
             <header class="mb-3">
                 <a href="#" class="burger-btn d-block d-xl-none">
@@ -56,7 +56,7 @@ $user = mysqli_fetch_all($result2, MYSQLI_ASSOC);
                 <section class="section">
                     <div class="card">
                         <div class="card-body">
-                        
+
                             <table class="table table-striped" id="table1">
                                 <thead>
                                     <tr>
@@ -65,10 +65,11 @@ $user = mysqli_fetch_all($result2, MYSQLI_ASSOC);
                                         <th>Email</th>
                                         <th>Role</th>
                                         <th>Hospital</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    
+
                                     <?php foreach ($user as $rows) : ?>
                                         <tr>
                                             <td><?php echo $rows["first_name"]; ?> <?php echo $rows["last_name"]; ?></td>
@@ -76,75 +77,81 @@ $user = mysqli_fetch_all($result2, MYSQLI_ASSOC);
                                             <td><?php echo $rows["email"]; ?></td>
                                             <td><?php echo $rows["role"]; ?></td>
                                             <td><?php echo $rows["hospital"]; ?></td>
+                                            <td>
+                                                <div class="btn-group mb-3 btn-group-sm" role="group" aria-label="Basic example">
+                                                    <button type="button" class="btn btn-primary"><i class="bi bi-pencil-square"></i></button>
+                                                    <button type="button" class="btn btn-primary"><i class="bi bi-x-octagon"></i></button>
+                                                </div>
+                                            </td>
                                         </tr>
-                                            <?php endforeach; ?>
+                                    <?php endforeach; ?>
                                 </tbody>
                             </table>
                             <button type="button" class="btn btn-primary mb-3" style="position: relative;float: right;margin-top: 8px;" data-bs-toggle="modal" data-bs-target="#inlineForm">
                                 Add Users
                             </button>
                             <div class="modal fade text-left" id="inlineForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel33" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title" id="myModalLabel33">Hospital Form </h4>
-                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                        <i data-feather="x"></i>
-                                    </button>
+                                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="myModalLabel33">Hospital Form </h4>
+                                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                                <i data-feather="x"></i>
+                                            </button>
+                                        </div>
+                                        <form action="function.php" method="POST">
+                                            <input type="hidden" name="command" value="ADD_USER">
+                                            <div class="modal-body">
+                                                <div class="form-group">
+                                                    <input type="text" placeholder="First Name" class="form-control" name="firstname">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" placeholder="Last Name" class="form-control" name="lastname">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" placeholder="Username" class="form-control" name="username">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" placeholder="Email" class="form-control" name="email">
+                                                </div>
+                                                <div class="form-group">
+                                                    <select class="choices form-select" name="role" required>
+                                                        <option value="">Role</option>
+                                                        <option value="admin">Admin</option>
+                                                        <option value="receptionist">Receptionist</option>
+                                                        <option value="financial manager">Financial Manager</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group">
+                                                    <select class="choices form-select" name="hospital" required>
+                                                        <option value="">Select Hospital</option>
+                                                        <?php foreach ($hospital as $rows) : ?>
+                                                            <option value="<?php echo $rows["hosp_name"]; ?>"><?php echo $rows["hosp_name"]; ?></option>
+
+
+                                                        <?php endforeach; ?>
+                                                    </select>
+
+                                                </div>
+                                                <p data-bs-toggle="modal" data-bs-target="#inlineForm" class="font-bold">Hospital Not Listed? Please Register the hospital first</p>
+                                                <div class="form-group">
+                                                    <input type="password" placeholder="Password" class="form-control" name="password">
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                                                    <i class="bx bx-x d-block d-sm-none"></i>
+                                                    <span class="d-none d-sm-block">Close</span>
+                                                </button>
+                                                <button type="submit" class="btn btn-primary ml-1">
+                                                    <i class="bx bx-check d-block d-sm-none"></i>
+                                                    <span class="d-none d-sm-block">Submit</span>
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
-                                <form action="function.php" method="POST">
-                                    <input type="hidden" name="command" value="ADD_USER">
-                                    <div class="modal-body">
-                                        <div class="form-group">
-                                            <input type="text" placeholder="First Name" class="form-control" name="firstname">
-                                        </div>
-                                         <div class="form-group">
-                                            <input type="text" placeholder="Last Name" class="form-control" name="lastname">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" placeholder="Username" class="form-control" name="username">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" placeholder="Email" class="form-control" name="email">
-                                        </div>
-                                        <div class="form-group">
-                                        <select class="choices form-select" name="role" required>
-                                            <option value="">Role</option>
-                                            <option value="admin">Admin</option>
-                                            <option value="receptionist">Receptionist</option>
-                                            <option value="financial manager">Financial Manager</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <select class="choices form-select" name="hospital" required>
-                                            <option value="">Select Hospital</option>
-                                            <?php foreach ($hospital as $rows) : ?>
-                                                <option value="<?php echo $rows["hosp_name"]; ?>"><?php echo $rows["hosp_name"]; ?></option>
-
-
-                                            <?php endforeach; ?>
-                                        </select>
-
-                                    </div>
-                                    <p data-bs-toggle="modal" data-bs-target="#inlineForm" class="font-bold">Hospital Not Listed? Please Register the hospital first</p>
-                                        <div class="form-group">
-                                            <input type="password" placeholder="Password" class="form-control" name="password">
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
-                                            <i class="bx bx-x d-block d-sm-none"></i>
-                                            <span class="d-none d-sm-block">Close</span>
-                                        </button>
-                                        <button type="submit" class="btn btn-primary ml-1">
-                                            <i class="bx bx-check d-block d-sm-none"></i>
-                                            <span class="d-none d-sm-block">Submit</span>
-                                        </button>
-                                    </div>
-                                </form>
                             </div>
-                        </div>
-                    </div>
                         </div>
                     </div>
 
@@ -157,8 +164,7 @@ $user = mysqli_fetch_all($result2, MYSQLI_ASSOC);
                         <p>2021 &copy; Mazer</p>
                     </div>
                     <div class="float-end">
-                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                                href="http://ahmadsaugi.com">A. Saugi</a></p>
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a href="http://ahmadsaugi.com">A. Saugi</a></p>
                     </div>
                 </div>
             </footer>
