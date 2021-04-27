bff034ae9f2f34063a699456bc5cdc228ef4fa8d
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-27 13:10:47 +0800

    Update user-profile.php

--- user-profile.php
+++ user-profile.php
@@ -20,18 +20,25 @@ if (isset($_POST['saveprofile'])) {
     $lastnameuser = $_POST['lastnameuser'];
     $emailuser = $_POST['emailuser'];
     $hospitaluser = $_POST['hospitaluser'];
-
     $image=time() . '-' . $_FILES['userimage']['name'];
-    $imageDir=$_FILES['userimage']['tmp_name'];
 
-    $upload_dir = 'images/';
-    $imageExt = strtolower(pathinfo($image,PATHINFO_EXTENSION));
+    //upload the image to a specific folder first and this folder for example called (images)
 
-    move_uploaded_file($imageDir,$upload_dir.$image);
+    $target_dir="images/";
+    $target_file=$target_dir . basename($image);
 
-    $sql = "UPDATE user SET first_name = '$firstnameuser', last_name = '$lastnameuser', email = '$emailuser', hospital = '$hospitaluser', user_profile = '$image'";
-    if (mysqli_query($conn,$sql)) {
-        echo '<script>alert("Successfully update profile");</script>';
+    //now move the image to the folder (images)
+
+    // move_uploaded_file($_FILES['image']['tmp_name'], $target_file);
+
+    //now let we upload the image to the database
+
+    if(move_uploaded_file($_FILES['userimage']['tmp_name'], $target_file)){
+
+        $sql = "UPDATE user SET first_name = '$firstnameuser', last_name = '$lastnameuser', email = '$emailuser', hospital = '$hospitaluser', user_profile = '$image'";
+        if (mysqli_query($conn,$sql)) {
+            echo '<script>alert("Successfully update profile");</script>';
+        }
         header("Location: auth-login.php");
     }
 }
