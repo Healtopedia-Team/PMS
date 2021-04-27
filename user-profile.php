80625eead9f374e462da0ce0ec6fc7fb71eea237
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-27 13:18:12 +0800

    Update user-profile.php

--- user-profile.php
+++ user-profile.php
@@ -91,7 +91,7 @@ if (isset($_POST['saveprofile'])) {
                                 <form method="POST">
                                 <button type="button" class="btn btn-outline-secondary" data-bs-toggle="tooltip" data-bs-placement="left" title="click to change" onclick="functiontry()" style="border-radius: 50%;">
                                         <img src="<?php echo 'images/'. $row['user_profile']; ?>" id="profileDisplay">
-                                        <input type="file" name="userimage" onChange="displayImage(this)" id="profileImage" class="form-control" accept='image/*'>
+                                        <input type="file" name="userimage" onChange="displayImage(this)" id="profileImage" class="form-control" style="display: none;" accept='image/*'>
                                 </button>
                                 <br>
                                 <center><br>
