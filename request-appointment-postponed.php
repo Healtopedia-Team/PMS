ccae554fd82ae32ede3f54e387282dc82b1c3f1c
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-28 16:40:44 +0800

    Update request-appointment-postponed.php

--- request-appointment-postponed.php
+++ request-appointment-postponed.php
@@ -3,17 +3,8 @@ include 'request-appointment-header.php';
 
 $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
 
-$result=mysqli_query($conn, "SELECT * FROM requestappoint WHERE req_status = 'postponed' ORDER BY request_id");
+$result=mysqli_query($conn, "SELECT * FROM requestappoint WHERE status = 'postponed' ORDER BY request_id");
 $data=mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-if (isset($_POST['reqpostpone'])) {
-    $requestid = $_POST['requestid'];
-    $sql = "UPDATE requestappoint SET req_status = 'postponed' WHERE request_id = '$requestid'";
-    if (mysqli_query($conn,$sql)) {
-        echo '<script>alert("Request rejected.");</script>';
-        echo '<script>window.location.href = "request-appointment-all.php";</script>';
-    }
-}
 ?>
 
                             <table class="table table-striped" id="table1">
@@ -51,6 +42,8 @@ if (isset($_POST['reqpostpone'])) {
                                                         <?php } ?>
 
                                                         <input type="text" name="requestid" value="<?php echo $row['request_id']; ?>" style="display: none;">
+
+                                                        <input type="text" name="requeststatus" value="<?php echo $row['request_id']; ?>" style="display: none;">
                                                 </form>
                                                         <a href="request-info.php" class="btn btn-info"><i class="bi bi-search"></i></a>
                                                     </div>
@@ -63,6 +56,45 @@ if (isset($_POST['reqpostpone'])) {
                     </div>
                 </section>
             </div>
+            <?php
+            if (isset($_POST['reqpostpone'])) {
+                $status = $_POST['requeststatus'];
+                if ($status != 'postponed') {
+                    $requestid = $_POST['requestid'];
+                    $sql = "UPDATE requestappoint SET req_status = 'postponed' WHERE request_id = '$requestid'";
+                    if (mysqli_query($conn,$sql)) {
+                        echo '<script>alert("Request rejected.");</script>';
+                        echo '<script>window.location.href = "request-appointment-all.php";</script>';
+                    }
+                }else{ ?>
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalCenterTitle">Vertically Centered</h5>
+                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                        <i data-feather="x"></i>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>
+                                        Croissant jelly-o halvah chocolate sesame snaps. Brownie caramels candy canes chocolate cake marshmallow icing lollipop I love. Gummies macaroon donut caramels biscuit topping danish.
+                                    </p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                                        <i class="bx bx-x d-block d-sm-none"></i>
+                                        <span class="d-none d-sm-block">Close</span>
+                                    </button>
+                                    <button type="button" class="btn btn-primary ml-1" data-bs-dismiss="modal">
+                                        <i class="bx bx-check d-block d-sm-none"></i>
+                                        <span class="d-none d-sm-block">Accept</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php }}?>
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
