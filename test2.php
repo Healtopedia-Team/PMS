26e1dade61e76d9489ba39e72fabd9f9d6ba2f6f
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-28 15:05:12 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<form action="upload.php" method="post"enctype="multipart/form-data">
+<form action="test3.php" method="post"enctype="multipart/form-data">
     Select the image you want to upload:
     <input type="file" name="file_to_upload"id="file_to_upload">
     <input type="submit" value="image upload"name="submit">
