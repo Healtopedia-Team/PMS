19501a7223f64cbe408a04819810587e185072d7
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-28 15:13:48 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,12 +1,63 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Image Preview and Upload PHP</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="main.css">
+</head>
 <body>
+  <div class="container">
+    <div class="row">
+      <div class="col-4 offset-md-4 pt-3 form-div">
+        <form action="test3.php" method="post" enctype="multipart/form-data">
+          <div class="form-group text-center" style="position: relative;" >
+            <span class="img-div">
+              <div class="text-center img-placeholder"  onClick="triggerClick()">
+                <h4>Upload image</h4>
+              </div>
+              <img src="avatar.jfif" onClick="triggerClick()" id="profileDisplay">
+            </span>
+            <input type="file" name="image" onChange="displayImage(this)" id="profileImage" class="form-control" style="display: none;" required accept='image/*'>
+            <label>Profile Image</label>
+          </div>
+          <div class="form-group">
+            <label>name</label>
+            <textarea name="name" class="form-control" required></textarea>
+          </div>
+          <div class="form-group">
+            <label>price</label>
+            <input name="price" class="form-control" required>
+          </div>
+          <div class="form-group">
+            <button type="submit" name="save_profile" class="btn btn-primary btn-block">Save User</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 
-<form action="test3.php" method="post"enctype="multipart/form-data">
-    Select the image you want to upload:
-    <input type="file" name="file_to_upload"id="file_to_upload">
-    <input type="submit" value="image upload"name="submit">
-</form>
+
+  <script>
+    function triggerClick(e) {
+  document.querySelector('#profileImage').click();
+}
+function displayImage(e) {
+  if (e.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e){
+      document.querySelector('#profileDisplay').setAttribute('src', e.target.result);
+    }
+    reader.readAsDataURL(e.files[0]);
+  }
+}
+</script>
+
+
+
 
 </body>
 </html>
