ca983c6d49c370813c70a9d4fbcebcd67db72a27
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-04-30 14:45:22 +0800

    Update request-appointment-postponed.php

--- request-appointment-postponed.php
+++ request-appointment-postponed.php
@@ -45,10 +45,47 @@ if (isset($_POST['reqpostpone'])) {
 
                                                         <input type="text" name="requeststatus" value="<?php echo $row['req_status']; ?>" style="display: none;">
                                                 </form>
-                                                        <a href="request-info.php" class="btn btn-info"><i class="bi bi-search"></i></a>
+                                                         <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#patient<?php echo $row['request_id']; ?>"><i class="bi bi-search"></i></button>
                                                     </div>
                                             </td>
                                         </tr>
+                                        <div class="modal fade text-left" id="patient<?php echo $row['request_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="myModalLabel1">Request Information</h5>
+                                                        <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
+                                                            <i data-feather="x"></i>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <label><b>Package Name</b>     : <?php echo $row['req_packname']; ?></label>
+                                                        <br>
+                                                        <label><b>Client Name</b>      : <?php echo $row['req_custname']; ?></label>
+                                                        <br>
+                                                        <label><b>IC/Passport</b>      : <?php echo $row['req_custid']; ?></label>
+                                                        <br>
+                                                        <label><b>No Phone</b>         : <?php echo $row['req_custphone']; ?></label>
+                                                        <br>
+                                                        <label><b>Address</b>          : <?php echo $row['req_custaddress']; ?></label>
+                                                        <br>
+                                                        <label><b>Nationalities</b>    : <?php echo $row['req_custnational']; ?></label>
+                                                        <br>
+                                                        <label><b>Appointment Date</b> : <?php echo $row['req_appdate']; ?></label>
+                                                        <br>
+                                                        <label><b>Appointment Time</b> : <?php echo $row['req_apptime']; ?></label>
+                                                        <br>
+                                                        <label><b>Status</b>           : <?php echo $row['req_status']; ?></label>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn" data-bs-dismiss="modal">
+                                                            <i class="bx bx-x d-block d-sm-none"></i>
+                                                            <span class="d-none d-sm-block">Close</span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     <?php } ?>
                                 </tbody>
                             </table>
@@ -56,45 +93,6 @@ if (isset($_POST['reqpostpone'])) {
                     </div>
                 </section>
             </div>
-            <?php
-            if (isset($_POST['reqpostpone'])) {
-                $status = $_POST['requeststatus'];
-                if ($status != 'postponed') {
-                    $requestid = $_POST['requestid'];
-                    $sql = "UPDATE requestappoint SET req_status = 'postponed' WHERE request_id = '$requestid'";
-                    if (mysqli_query($conn,$sql)) {
-                        echo '<script>alert("Request postponed.");</script>';
-                        echo '<script>window.location.href = "request-appointment-postponed.php";</script>';
-                    }
-                }else{ ?>
-                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalCenterTitle">Vertically Centered</h5>
-                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                        <i data-feather="x"></i>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>
-                                        Croissant jelly-o halvah chocolate sesame snaps. Brownie caramels candy canes chocolate cake marshmallow icing lollipop I love. Gummies macaroon donut caramels biscuit topping danish.
-                                    </p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
-                                        <i class="bx bx-x d-block d-sm-none"></i>
-                                        <span class="d-none d-sm-block">Close</span>
-                                    </button>
-                                    <button type="button" class="btn btn-primary ml-1" data-bs-dismiss="modal">
-                                        <i class="bx bx-check d-block d-sm-none"></i>
-                                        <span class="d-none d-sm-block">Accept</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php }}?>
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
