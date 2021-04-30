65954306c765b6d2e9737a66ebf95a53cb970abc
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-30 14:50:56 +0800

    Update test.php

--- test.php
+++ test.php
@@ -25,7 +25,7 @@
         <p>
             <input type="file" name="photo" onchange="previewFile(this);" required>
         </p>
-        <img id="previewImg" src="images/avatar.jpg" alt="Placeholder">
+        <img id="previewImg" src="" alt="Placeholder">
         <p>
             <input type="submit" value="Submit">
         </p>
