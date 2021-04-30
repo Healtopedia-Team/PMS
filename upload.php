22f90e3bfa137b38f8503c413bf4bf22f1e4c005
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-30 14:26:41 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html>
-<body>
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["file_to_upload"]["name"]);
+$upload_ok = 1;
+$image_file_type = pathinfo($target_file,PATHINFO_EXTENSION);
+// checking if image file is accessible
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["file_to_upload"]["tmp_name"]);
+    if($check !== false) {
+        echo "The file you picked is an image - " . $check["mime"] . ".";
+        $upload_ok = 1;
+    } else {
+        echo "The file you picked is not image.";
+        $upload_ok = 0;
+    }
+}
+?>
 
-<form action="upload.php" method="post"enctype="multipart/form-data">
-    Select the image you want to upload:
-    <input type="file" name="file_to_upload"id="file_to_upload">
-    <input type="submit" value="image upload"name="submit">
-</form>
-
-</body>
-</html>
