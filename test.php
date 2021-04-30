a292325f313fa4d15addea0a21ccec1444a1b131
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-04-30 14:46:14 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>jQuery Preview an Image Before it is Uploaded</title>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script>
+    function previewFile(input){
+        var file = $("input[type=file]").get(0).files[0];
+ 
+        if(file){
+            var reader = new FileReader();
+ 
+            reader.onload = function(){
+                $("#previewImg").attr("src", reader.result);
+            }
+ 
+            reader.readAsDataURL(file);
+        }
+    }
+</script>
+</head> 
 <body>
-
-<form action="upload.php" method="post"enctype="multipart/form-data">
-    Select the image you want to upload:
-    <input type="file" name="file_to_upload"id="file_to_upload">
-    <input type="submit" value="image upload"name="submit">
-</form>
-
+    <form action="confirmation.php" method="post">
+        <p>
+            <input type="file" name="photo" onchange="previewFile(this);" required>
+        </p>
+        <img id="previewImg" src="/examples/images/transparent.png" alt="Placeholder">
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+    </form>
 </body>
 </html>
+
