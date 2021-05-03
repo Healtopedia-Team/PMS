ca53dc4081ad6f23e19e744c76cf559dd0be4fa9
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-03 17:20:01 +0800

    another commit

--- request-addappoint.php
+++ request-addappoint.php
@@ -146,7 +146,7 @@ if(!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true){
                     if (isset($_POST['submitdate'])) {
                         $sql = "DELETE FROM `requestappoint` WHERE req_packname IS NULL";
                         if(mysqli_query($conn,$sql)){
-                            $appdate = $_POST['datecheck'];
+                            static $appdate = $_POST['datecheck'];
                             $sql2 = "INSERT INTO requestappoint SET req_appdate = '$appdate', request_count = '1'";
                             if (mysqli_query($conn,$sql2)) {
                                 $last_id = mysqli_insert_id($conn);
@@ -168,8 +168,7 @@ if(!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true){
                         $national=$_POST['national'];
                         $apptime=$_POST['apptime'];
                         $latestid=$_POST['latestid'];
-                        
-                        $appdate = $_POST['datecheck'];
+
                         $register_name = $_SESSION["name"];
                         $t = time();
                         $current_timestamp = date("Y-m-d H:i", $t);
