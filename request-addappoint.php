01602390b005c44c88cc22eb3f745edcbb87182c
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-03 16:57:10 +0800

    another commit

--- request-addappoint.php
+++ request-addappoint.php
@@ -168,20 +168,23 @@ if(!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true){
                         $national=$_POST['national'];
                         $apptime=$_POST['apptime'];
                         $latestid=$_POST['latestid'];
+                        /*
                         $current_timestamp = time();
                         $finalDateD = date('Y-m-d', strtotime($appdate));
                         $finalDateT = date('H:i', strtotime($apptime));
                         $reservedTime = $finalDateD . ' ' . $finalDateT;
                         $message = 'Inserting...';
+                        */
                         $sql = "UPDATE requestappoint SET req_packname = '$packname', req_custname = '$name', req_custid = '$passport', req_custaddress = '$address', req_custphone = '$phoneno', req_custnational = '$national', req_apptime = '$apptime', req_status = 'pending' WHERE  request_id = '$latestid'";
-                        $query = "INSERT INTO 'notification' (`id`, `name`, `type`, `message`, `status`, `date`, `reserved_date`) VALUES (10, '', 'comment', 'Hi crush', 'read', '2021-05-03 16:43:10', '2021-05-28 16:41:41')";
+                        $query = "INSERT INTO notification (`id`, `name`, `type`, `message`, `status`, `date`, `reserved_date`) VALUES (10, '', 'comment', 'Hi crush', 'read', '2021-05-03 16:43:10', '2021-05-28 16:41:41')";
                         if (mysqli_query($conn,$sql) and mysqli_query($conn, $query)) {
                             echo '<script>
                             alert("Successfully request an appointment !!");
                             </script>';
                         }else{
+                            $var_error = mysqli_error($conn);
                             echo '<script>
-                            alert("Request failed !!");
+                            alert($var_error);
                             </script>';
                         }
                     }
