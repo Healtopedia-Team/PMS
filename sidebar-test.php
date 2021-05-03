2684ba86f19a4e3b8808c1c80067a8504af2e728
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-03 18:31:03 +0800

    another commit

--- sidebar-test.php
+++ sidebar-test.php
@@ -161,7 +161,7 @@ session_start();
                             <?php
                             $query = "SELECT * from 'notification' order by `date` DESC";
                             $notifications = mysqli_fetch_assoc($query);
-                            if ($cnt_not>  0) {
+                            if ($cnt_not ==  0) {
                                 foreach ($notifications as $i) {
                             ?>
                                     <li>
