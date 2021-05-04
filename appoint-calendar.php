aa8ffcd9c0929a9a2d94bd339a37a046b1492774
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-04 11:20:48 +0800

    Update appoint-calendar.php

--- appoint-calendar.php
+++ appoint-calendar.php
@@ -25,50 +25,6 @@
             selectable:true,
             selectHelper:true,
             editable:true,
-            eventResize:function(event){
-                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                var title = event.title;
-                var id = event.id;
-                $.ajax({
-                    url:"update.php",
-                    type:"POST",
-                    data:{title:title, start:start, end:end, id:id},
-                    success:function(){
-                        calendar.fullCalendar('refetchEvents');
-                        alert('Event Update');
-                    }
-                })
-            },
-            eventDrop:function(event){
-                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                var title = event.title;
-                var id = event.id;
-                $.ajax({
-                    url:"update.php",
-                    type:"POST",
-                    data:{title:title, start:start, end:end, id:id},
-                    success:function(){
-                        calendar.fullCalendar('refetchEvents');
-                        alert("Event Updated");
-                    }
-                });
-            },
-            eventClick:function(event){
-                if(confirm("Are you sure you want to remove it?")){
-                    var id = event.id;
-                    $.ajax({
-                        url:"delete.php",
-                        type:"POST",
-                        data:{id:id},
-                        success:function(){
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Removed");
-                        }
-                    })
-                }
-            },
         });
     });
 </script>
