b6c1780c906e3417c7504a4294100a17ea9bbf45
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-04 12:01:16 +0800

    another commit

--- sidebar-test.php
+++ sidebar-test.php
@@ -171,7 +171,8 @@ $username = $_SESSION['name'];
                             </span>
                         </a>
                         <!-- Here not done -->
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton" style="width: 300px !important;
+                        height: 400px !important;">
                             <li>
                                 <h6 class="dropdown-header">Notifications</h6>
                             </li>
