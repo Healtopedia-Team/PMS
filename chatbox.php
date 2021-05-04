2724a883309bd2e901fbc59490c0825e3d2f6c15
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-04 16:42:01 +0800

    chatbox design --- first commit

--- chatbox.php
+++ chatbox.php
@@ -85,7 +85,7 @@ Website: http://emilcarlsson.se/
 					if (mysqli_num_rows($sql) > 0) {
 						$row = mysqli_fetch_assoc($sql);
 					} else {
-						echo "ERROR: " . mysqli_error($conn);
+						echo $username;
 					}
 					?>
 					<?= console_log($username); ?>
