6509804f994df858ad55f1f646180c587a43c81e
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-04 11:18:39 +0800

    Update appoint-calendar.php

--- appoint-calendar.php
+++ appoint-calendar.php
@@ -25,10 +25,10 @@
             selectable:true,
             selectHelper:true,
             select: function(start, end, allDay){
-               var title = prompt("Enter Event Title");
+                var title = prompt("Enter Event Title");
                 if(title){
                     var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-                   var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+                    var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
                     $.ajax({
                         url:"insert.php",
                         type:"POST",
@@ -39,52 +39,52 @@
                         }
                     })
                 }
-            };
-            //editable:true,
-            //eventResize:function(event){
-            //    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-            //    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-            //    var title = event.title;
-            //    var id = event.id;
-            //    $.ajax({
-            //        url:"update.php",
-            //        type:"POST",
-            //        data:{title:title, start:start, end:end, id:id},
-            //        success:function(){
-            //            calendar.fullCalendar('refetchEvents');
-            //            alert('Event Update');
-            //        }
-            //    })
-            //},
-            //eventDrop:function(event){
-            //    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-            //    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-            //    var title = event.title;
-            //    var id = event.id;
-            //    $.ajax({
-            //        url:"update.php",
-            //        type:"POST",
-            //        data:{title:title, start:start, end:end, id:id},
-            //        success:function(){
-            //            calendar.fullCalendar('refetchEvents');
-            //            alert("Event Updated");
-            //        }
-            //    });
-            //},
-            //eventClick:function(event){
-            //    if(confirm("Are you sure you want to remove it?")){
-            //        var id = event.id;
-            //        $.ajax({
-            //            url:"delete.php",
-            //            type:"POST",
-            //            data:{id:id},
-            //            success:function(){
-            //                calendar.fullCalendar('refetchEvents');
-            //                alert("Event Removed");
-            //            }
-            //        })
-            //    }
-            //},
+            },
+            editable:true,
+            eventResize:function(event){
+                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+                var title = event.title;
+                var id = event.id;
+                $.ajax({
+                    url:"update.php",
+                    type:"POST",
+                    data:{title:title, start:start, end:end, id:id},
+                    success:function(){
+                        calendar.fullCalendar('refetchEvents');
+                        alert('Event Update');
+                    }
+                })
+            },
+            eventDrop:function(event){
+                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+                var title = event.title;
+                var id = event.id;
+                $.ajax({
+                    url:"update.php",
+                    type:"POST",
+                    data:{title:title, start:start, end:end, id:id},
+                    success:function(){
+                        calendar.fullCalendar('refetchEvents');
+                        alert("Event Updated");
+                    }
+                });
+            },
+            eventClick:function(event){
+                if(confirm("Are you sure you want to remove it?")){
+                    var id = event.id;
+                    $.ajax({
+                        url:"delete.php",
+                        type:"POST",
+                        data:{id:id},
+                        success:function(){
+                            calendar.fullCalendar('refetchEvents');
+                            alert("Event Removed");
+                        }
+                    })
+                }
+            },
         });
     });
 </script>
