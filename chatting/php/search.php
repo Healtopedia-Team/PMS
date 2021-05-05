082dd2edc29971365ec2dfe6e7b0f3b3eebd2168
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-05 12:30:50 +0800

    chatbox sidebar friendlist commit

--- chatting/php/search.php
+++ chatting/php/search.php
@@ -1,6 +1,4 @@
 <?php
-    session_start();
-    include_once "../dbconnect.php";
 
     $outgoing_id = $_SESSION['user_id'];
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
