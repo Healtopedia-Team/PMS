b782af71699074494eb5582abe53455073bc4c49
heavenluv <weipj128@gmail.com>
2021-05-05 20:19:58 +0800

    chatbox sidebar friendlist commit

--- chatting/searchbar.php
+++ chatting/searchbar.php
@@ -9,7 +9,7 @@
     }
     $safe_value = mysqli_real_escape_string($conn, $_POST['searchvalue']);
    
-    $result = mysqli_query($conn, "SELECT * FROM user WHERE first_name LIKE '$safe_value%' OR last_name LIKE '$safe_value%'");
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE first_name LIKE '$safe_value' OR last_name LIKE '$safe_value'");
     if (mysqli_num_rows($query) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $output .= '<li class="contact">
