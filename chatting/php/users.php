e86f7435c0c93a685364865a4c8b37f9e53a792b
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-05 12:36:21 +0800

    chatbox sidebar friendlist commit

--- chatting/php/users.php
+++ chatting/php/users.php
@@ -1,11 +1,20 @@
 <?php
-    $outgoing_id = $_SESSION['user_id'];
-    $sql = "SELECT * FROM user WHERE NOT user_id = {$outgoing_id} ORDER BY user_id DESC";
-    $query = mysqli_query($conn, $sql);
+    $outgoing_id = $user_id;
+    $sqlu = "SELECT * FROM user WHERE NOT user_id = {$outgoing_id} ORDER BY user_id DESC";
+    $userlist = mysqli_query($conn, $sqlu);
     $output = "";
-    if(mysqli_num_rows($query) == 0){
-        $output .= "No users are available to chat";
-    } elseif(mysqli_num_rows($query) > 0){
+    if(mysqli_num_rows($userlist) == 0){
+        $output .= '<li class="contact">
+                        <div class="wrap">
+                            <span class="contact-status online"></span>
+                            <img src= "../assets/images/faces/1.jpg" alt="" />
+                            <div class="meta">
+                                <p class="name">Nobody</p>
+                                <p class="preview"> No users are available to chat</p>
+                            </div>
+                        </div>
+                    </li>';
+    } elseif(mysqli_num_rows($userlist) > 0){
         include_once "data.php";
     }
     echo $output;
