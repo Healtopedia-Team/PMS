08111ef35019ac68fc29468d9fe17573f7bed2c2
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-05 17:49:15 +0800

    chatbox sidebar friendlist commit

--- chatting/searchbar.php
+++ chatting/searchbar.php
@@ -3,7 +3,7 @@
 <?php
     $safe_value = $_POST['searchvalue'];
    
-    $result = mysqli_query($conn, "SELECT * FROM user WHERE first_name LIKE '$safe_value%' OR last_name LIKE '$safe_value%'");
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE first_name LIKE '$safe_value' OR last_name LIKE '$safe_value'");
     if (mysqli_num_rows($query) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $output .= '<li class="contact">
