9dc9a05bc4ad933e337bcf4cd96ad5523dd64e41
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-05 15:11:57 +0800

    chatbox sidebar friendlist commit

--- chatting/chatbox.php
+++ chatting/chatbox.php
@@ -117,66 +117,6 @@ Website: http://emilcarlsson.se/
 			</div>
 			<div id="contacts">
 				<ul class="users-list">
-					<?php
-					$outgoing_id = $user_id;
-					$sql = "SELECT * FROM user WHERE NOT user_id = '$outgoing_id' ORDER BY user_id DESC";
-					$query = mysqli_query($conn, $sql);
-					$output = "";
-					?>
-					<?= console_log(mysqli_fetch_assoc($query)); ?>
-					<?php
-					if (mysqli_num_rows($query) == 0) {
-						$output .= '<li class="contact">
-											<div class="wrap">
-												<span class="contact-status offline"></span>
-												<img src= "../assets/images/faces/1.jpg" alt="" />
-												<div class="meta">
-													<p class="name">Nobody</p>
-													<p class="preview"> No users are available to chat</p>
-												</div>
-											</div>
-										</li>';
-					} elseif (mysqli_num_rows($query) > 0) {
-						while ($row = mysqli_fetch_assoc($query)) {
-							$sql2 = "SELECT * FROM chat WHERE (incoming_msg_id = {$row['user_id']}
-										OR outgoing_msg_id = {$row['user_id']}) AND (outgoing_msg_id = {$outgoing_id} 
-										OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
-
-							$query2 = mysqli_query($conn, $sql2);
-							$row2 = mysqli_fetch_assoc($query2);
-							if (mysqli_num_rows($query2) > 0) {
-								$result = $row2['msg'];
-							} else {
-								$result = "No message available";
-							}
-							if (strlen($result) > 28) {
-								$msg =  substr($result, 0, 28) . '...';
-							} else {
-								$msg = $result;
-							}
-							if (isset($row2['outgoing_msg_id'])) {
-								($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
-							} else {
-								$you = "";
-							}
-							//($outgoing_id == $row['user_id']) ? $hid_me = "hide" : $hid_me = "";
-
-							$output .= '
-											<li class="contact">
-												<div class="wrap">
-													<span class="contact-status online"></span>
-													<img src= "../assets/images/faces/1.jpg" alt="" />
-													<div class="meta">
-														<p class="name">' . $row['first_name'] . " " . $row['last_name'] . '</p>
-														<p class="preview"> ' . $you . $msg . '</p>
-													</div>
-												</div>
-											</li>
-										';
-						}
-					}
-					echo $output;
-					?>
 				</ul>
 			</div>
 			<div id="bottom-bar">
@@ -308,11 +248,32 @@ Website: http://emilcarlsson.se/
 			}
 		});
 		//# sourceURL=pen.js
+		const phpscript = "<?php
+							$outgoing_id = 10;
+							$sqlu = "SELECT * FROM user WHERE NOT user_id='10' ORDER BY user_id DESC";
+							$userlist = mysqli_query($conn, $sqlu);
+							$output = "";
+							console_log($outgoing_id);
+							if (mysqli_num_rows($userlist) == 0) {
+								$output .= '<li class="contact">
+                        <div class="wrap">
+                            <div class="meta">
+                                <p class="name">Nobody</p>
+                                <p class="preview"> No users are available to chat</p>
+                            </div>
+                        </div>
+                    </li>';
+							} elseif (mysqli_num_rows($userlist) > 0) {
+								include_once "data.php";
+							}
+							echo $output;
+							?>";
+
 
 		const usersList = document.querySelector('.users-list')
 		setInterval(() => {
 			let xhr = new XMLHttpRequest()
-			xhr.open('GET', "php/users.php", true)
+			xhr.open('GET', phpscript, true)
 			xhr.onload = () => {
 				if (xhr.readyState === XMLHttpRequest.DONE) {
 					if (xhr.status === 200) {
@@ -327,11 +288,12 @@ Website: http://emilcarlsson.se/
 			}
 			xhr.send()
 		}, 500)
+
 		const searchBar = document.querySelector('#search input')
 		searchBar.onkeyup = () => {
 			let searchTerm = searchBar.value
 			let xhr = new XMLHttpRequest()
-			xhr.open('POST', phpscript, true)
+			xhr.open('POST', 'php/search.php', true)
 			xhr.onload = () => {
 				if (xhr.readyState === XMLHttpRequest.DONE) {
 					if (xhr.status === 200) {
