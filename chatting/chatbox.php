96afd98ef765454a0ac7dd73053b683aab4c08bc
Pun Jun Wei <heavenluv@Puns-MacBook-Air.local>
2021-05-05 12:49:52 +0800

    chatbox sidebar friendlist commit

--- chatting/chatbox.php
+++ chatting/chatbox.php
@@ -125,42 +125,6 @@ Website: http://emilcarlsson.se/
 				<ul class="users-list">
 
 				</ul>
-				<script>
-					const usersList = document.querySelector('.users-list')
-					setInterval(() => {
-						let xhr = new XMLHttpRequest()
-						xhr.open('GET', 'php/users.php', true)
-						xhr.onload = () => {
-							if (xhr.readyState === XMLHttpRequest.DONE) {
-								if (xhr.status === 200) {
-									let data = xhr.response
-									if (!searchBar.classList.contains('active')) {
-										usersList.innerHTML = data
-									}
-								}
-							}
-						}
-						xhr.send()
-					}, 500)
-
-					const searchBar = document.querySelector('#search input')
-					searchBar.onkeyup = () => {
-						let searchTerm = searchBar.value
-						let xhr = new XMLHttpRequest()
-						xhr.open('POST', 'php/search.php', true)
-						xhr.onload = () => {
-							if (xhr.readyState === XMLHttpRequest.DONE) {
-								if (xhr.status === 200) {
-									let data = xhr.response
-									//console.log(data);
-									usersList.innerHTML = data
-								}
-							}
-						}
-						xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-						xhr.send('searchTerm=' + searchTerm)
-					}
-				</script>
 			</div>
 			<div id="bottom-bar">
 				<button id="addcontact"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> <span>Add contact</span></button>
@@ -291,6 +255,41 @@ Website: http://emilcarlsson.se/
 			}
 		});
 		//# sourceURL=pen.js
+
+		const usersList = document.querySelector('.users-list')
+		setInterval(() => {
+			let xhr = new XMLHttpRequest()
+			xhr.open('GET', 'php/users.php', true)
+			xhr.onload = () => {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					if (xhr.status === 200) {
+						let data = xhr.response
+						if (!searchBar.classList.contains('active')) {
+							usersList.innerHTML = data
+						}
+					}
+				}
+			}
+			xhr.send()
+		}, 500)
+
+		const searchBar = document.querySelector('#search input')
+		searchBar.onkeyup = () => {
+			let searchTerm = searchBar.value
+			let xhr = new XMLHttpRequest()
+			xhr.open('POST', 'php/search.php', true)
+			xhr.onload = () => {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					if (xhr.status === 200) {
+						let data = xhr.response
+						//console.log(data);
+						usersList.innerHTML = data
+					}
+				}
+			}
+			xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+			xhr.send('searchTerm=' + searchTerm)
+		}
 	</script>
 </body>
 
