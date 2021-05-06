47bdb17104be6a95149668629686216380b4b986
heavenluv <weipj128@gmail.com>
2021-05-06 10:47:58 +0800

    chatbox sidebar friendlist commit

--- chatting/chatbox.php
+++ chatting/chatbox.php
@@ -23,13 +23,8 @@ function console_log($output, $with_script_tags = true)
 ?>
 <?= console_log($username); ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src='../assets/vendors/jquery/jquery.min.js'></script>
 <!------ Include the above in your HEAD tag ---------->
 
 
@@ -37,9 +32,6 @@ function console_log($output, $with_script_tags = true)
 <html class=''>
 
 <head>
-	<script src='//production-assets.codepen.io/assets/editor/live/console_runner-079c09a0e3b9ff743e39ee2d5637b9216b3545af0de366d4b9aad9dc87e26bfd.js'></script>
-	<script src='//production-assets.codepen.io/assets/editor/live/events_runner-73716630c22bbc8cff4bd0f07b135f00a0bdc5d14629260c3ec49e5606f98fdd.js'></script>
-	<script src='//production-assets.codepen.io/assets/editor/live/css_live_reload_init-2c0dc5167d60a5af3ee189d570b1835129687ea2a61bee3513dee3a50c115a77.js'></script>
 	<meta charset='UTF-8'>
 	<meta name="robots" content="noindex">
 	<link rel="shortcut icon" type="image/x-icon" href="//production-assets.codepen.io/assets/favicon/favicon-8ea04875e70c4b0bb41da869e81236e54394d63638a1ef12fa558a4a835f1164.ico" />
@@ -55,8 +47,8 @@ function console_log($output, $with_script_tags = true)
 			});
 		} catch (e) {}
 	</script>
-	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
-	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
+	<link rel='stylesheet prefetch' href='../PMS/assets/vendors/fontawesome/all.min.js'>
+	<link rel='stylesheet prefetch' href='../assets/vendors/fontawesome/all.min.css'>
 	<link rel="stylesheet" href="chat.css">
 </head>
 
@@ -187,8 +179,6 @@ Website: http://emilcarlsson.se/
 		</div>
 	</div>
 	<script src="chat.js"></script>
-	<script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>
-	<script src='../assets/vendors/jquery/jquery.min.js'></script>
 	<script>
 		<?php
 		function listalluser($user_id, $conn)
