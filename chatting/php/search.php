3ca0da5783fb293ce367d853c65bf706aaba7d1c
heavenluv <weipj128@gmail.com>
2021-05-06 14:22:57 +0800

    chatbox sidebar friendlist commit

--- chatting/php/search.php
+++ chatting/php/search.php
@@ -6,8 +6,7 @@
     $output = "";
     $line = mysqli_query($conn, "SELECT * FROM user WHERE first_name LIKE '%$searchTerm%' OR last_name LIKE '%$searchTerm%'");
     //$line = "SELECT * FROM user WHERE first_name LIKE '%$searchTerm%'";
-
-    $searchquery = mysqli_query($conn, $line);
+    $searchquery = mysqli_query($conn, $line) or die(mysqli_error($conn));
     if(mysqli_num_rows($searchquery) > 0){
         while($row = mysqli_fetch_assoc($searchquery)){
             $output .=
