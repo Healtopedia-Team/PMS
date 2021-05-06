63c677f4bdacf777256c1fb894808567c05ca159
heavenluv <weipj128@gmail.com>
2021-05-06 13:16:43 +0800

    chatbox sidebar friendlist commit

--- chatting/php/search.php
+++ chatting/php/search.php
@@ -2,7 +2,8 @@
     session_start();
     include_once "dbconnect.php";
     $outgoing_id = $_SESSION['user_id'];
-    $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
+    //$searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
+    $searchTerm = $_POST['searchTerm'];
     $output = "";
     $query = mysqli_query($conn, "SELECT * FROM user WHERE first_name LIKE '%$searchTerm%' OR last_name LIKE '%$searchTerm%'");
     if(mysqli_num_rows($query) > 0){
