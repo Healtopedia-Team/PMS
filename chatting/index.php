3e3d785ac9e58218dd559e5c7704b8dd1f3e7354
heavenluv <weipj128@gmail.com>
2021-05-06 17:27:59 +0800

    chatbox sidebar friendlist commit

--- chatting/index.php
+++ chatting/index.php
@@ -115,7 +115,7 @@ Website: http://emilcarlsson.se/
 				<input type="text" placeholder="Search contacts..." id="search" name="searchvalue">
 			</div>
 			<div id="contacts">
-				<ul class="users-list" onclick="Clickuser()">
+				<ul class="users-list" onclick="selectUser(e)">
 
 				</ul>
 			</div>
