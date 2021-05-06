ab9f15ed9fa53e8635e5ef95337034c7e0e86d8c
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-06 12:41:08 +0800

    Update patient-report.php

--- patient-report.php
+++ patient-report.php
@@ -134,7 +134,7 @@ $data=mysqli_fetch_all($result, MYSQLI_ASSOC);
                                             </div>
                                         </div>
                             <!--========================================== M O D A L == U P L O A D == R E P O R T =====================================-->
-                                        <div class="modal fade text-left" id="postpone<?php echo $row['request_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel19" aria-hidden="true">
+                                        <div class="modal fade text-left" id="upload<?php echo $row['request_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel19" aria-hidden="true">
                                             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-sm" role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
