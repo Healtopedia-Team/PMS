2bbc7f916e40c3e1fa08f623b5a3d3cac6fc7454
hajarrafie <43933592+jajatiha@users.noreply.github.com>
2021-05-06 10:47:47 +0800

    test timestamp from index

--- time-test.php
+++ time-test.php
@@ -1,4 +1,9 @@
 <?php
-    $dt = new DateTime();
-    echo $dt->format('Y-m-d H:i:s');
-?>
+$dt = new DateTime();
+echo $dt->format('Y-m-d H:i:s');
+
+$timestamp = strtotime('today midnight +8 hour');
+$timestamp2 = strtotime('tomorrow midnight +8 hour');
+
+echo $timestamp;
+echo $timestamp2;
