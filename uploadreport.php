62f1779245e7b8de5c9c608d2b245ba33083cb3f
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-06 13:50:09 +0800

    Update uploadreport.php

--- uploadreport.php
+++ uploadreport.php
@@ -2,6 +2,7 @@
 include_once 'dbconnect.php';
 
 if(isset($_POST['requpreport'])) {
+    $requestid = $_POST['requestid'];
     $file = rand(1000,100000)."-".$_FILES['file']['name'];
     $file_loc = $_FILES['file']['tmp_name'];
     $file_size = $_FILES['file']['size'];
@@ -14,7 +15,8 @@ if(isset($_POST['requpreport'])) {
     $final_file=str_replace(' ','-',$new_file_name);
  
     if(move_uploaded_file($file_loc,$folder.$final_file)) {
-        $sql="INSERT INTO requestappointment(patient_report) VALUES('$final_file','$file_type','$new_size')";
+        $sql="INSERT INTO requestappointment(patient_report) VALUES('$final_file')";
+        $sql = "UPDATE requestappoint SET patient_report = '$final_file' WHERE request_id = '$requestid'";
         mysqli_query($conn,$sql);
     ?>
     <script>
