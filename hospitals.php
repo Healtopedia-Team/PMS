c9dff507a3d1f719bd620e09548489d77e584021
hajarrafie <43933592+jajatiha@users.noreply.github.com>
2021-05-06 12:08:45 +0800

    add hosp roles

--- hospitals.php
+++ hospitals.php
@@ -33,7 +33,7 @@ $hospital = mysqli_fetch_all($result, MYSQLI_ASSOC);
                 <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Users List</h3>
+                            <h3>Hospital List</h3>
                         </div>
                         <div class="col-12 col-md-6 order-md-2 order-first">
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
