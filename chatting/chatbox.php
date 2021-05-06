32f15b68f52930d147628cb602e6802d9907511a
heavenluv <weipj128@gmail.com>
2021-05-06 10:40:08 +0800

    chatbox sidebar friendlist commit

--- chatting/chatbox.php
+++ chatting/chatbox.php
@@ -188,7 +188,7 @@ Website: http://emilcarlsson.se/
 	</div>
 	<script src="chat.js"></script>
 	<script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>
-	<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+	<script src='../assets/vendors/jquery/jquery.min.js'></script>
 	<script>
 		<?php
 		function listalluser($user_id, $conn)
