a0b37efdd362fb560595e639921709e40296d69b
heavenluv <weipj128@gmail.com>
2021-05-06 10:50:00 +0800

    chatbox sidebar friendlist commit

--- chatting/chatbox.php
+++ chatting/chatbox.php
@@ -24,6 +24,11 @@ function console_log($output, $with_script_tags = true)
 <?= console_log($username); ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="../assets/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src='../assets/vendors/jquery/jquery.min.js'></script>
 <!------ Include the above in your HEAD tag ---------->
 
@@ -47,8 +52,8 @@ function console_log($output, $with_script_tags = true)
 			});
 		} catch (e) {}
 	</script>
-	<link rel='stylesheet prefetch' href='../PMS/assets/vendors/fontawesome/all.min.js'>
-	<link rel='stylesheet prefetch' href='../assets/vendors/fontawesome/all.min.css'>
+	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
+	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
 	<link rel="stylesheet" href="chat.css">
 </head>
 
