3826c65e44e8d00201bb4588a5d01f669322fba0
heavenluv <weipj128@gmail.com>
2021-05-06 14:27:04 +0800

    chatbox design --- first commit

--- chatting/php/search.php
+++ chatting/php/search.php
@@ -4,7 +4,8 @@
     $outgoing_id = $_SESSION['user_id'];
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
     $output = "";
-    $line = mysqli_query($conn, "SELECT * FROM user WHERE first_name LIKE '%$searchTerm' OR last_name LIKE '%$searchTerm'");
+    //LIKE '%$searchTerm' OR last_name LIKE '%$searchTerm'
+    $line = mysqli_query($conn, "SELECT * FROM user WHERE first_name='Hajar");
     //$line = "SELECT * FROM user WHERE first_name LIKE '%$searchTerm%'";
     $searchquery = mysqli_query($conn, $line);
     if(mysqli_num_rows($searchquery) > 0){
