05e9a206a9c17caaa76976284ac27bbbdaa553f0
heavenluv <weipj128@gmail.com>
2021-05-06 11:46:18 +0800

    chatbox sidebar friendlist commit

--- chatting/userlist.php
+++ chatting/userlist.php
@@ -2,8 +2,7 @@
     session_start();
     include_once "dbconnect.php";
     $outgoing_id = $_SESSION['unique_id'];
-    $find_other_user = "SELECT * FROM user WHERE NOT user_id = '$outgoing_id";
-    $listed_user = mysqli_query($conn, $find_other_user);
+    $listed_user = mysqli_query($conn, "SELECT * FROM user WHERE NOT user_id='$outgoing_id'");
     $output = "";
     if (mysqli_num_rows($listed_user) == 0) {
         /*
