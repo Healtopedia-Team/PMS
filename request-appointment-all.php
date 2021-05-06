4679e743678fd1ada544d81b68904d09b6a3e204
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-06 11:37:59 +0800

    Update request-appointment-all.php

--- request-appointment-all.php
+++ request-appointment-all.php
@@ -69,11 +69,11 @@ if (isset($_POST['reqcomplete'])) {
                                                              <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#reject<?php echo $row['request_id']; ?>"><i class="bi bi-x-circle"></i></button>
                                                         <?php } ?>
 
-                                                        <?php if ($row['req_status'] == "approved") {?>
+                                                        <?php if ($row['req_status'] == "approved" || $row['req_status'] == "postpone") {?>
                                                             <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#complete<?php echo $row['request_id']; ?>"><i class="bi bi-calendar3-week"></i></button>
                                                         <?php } ?>
 
-                                                        <?php if ($row['req_status'] == "approved" || $row['req_status'] == "postponed") {?>
+                                                        <?php if ($row['req_status'] == "approved") {?>
                                                             <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#postpone<?php echo $row['request_id']; ?>"><i class="bi bi-check2-circle"></i></button>
                                                         <?php } ?>
 
