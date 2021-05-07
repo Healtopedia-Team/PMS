21f8bc6faf52cb0b332e99d93dfae2553336606d
heavenluv <weipj128@gmail.com>
2021-05-07 20:54:51 +0800

    chatting area configuration

--- chatting/js/insert-chat.php
+++ chatting/js/insert-chat.php
@@ -5,6 +5,7 @@
     $incoming_id = mysqli_real_escape_string($conn, $_SESSION["target_chat_user"]);
     $message = mysqli_real_escape_string($conn, $_POST['message']);
     echo "The message is " . $message;
+    console_log($message);
     //echo $message;
     if (!empty($message)) {
         $sql = mysqli_query($conn, "INSERT INTO chat (incoming_msg_id, outgoing_msg_id, msg)
