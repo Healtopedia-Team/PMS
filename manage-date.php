1719a0c9f9c3b8cb5889696684e30f4ceda7e7cf
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-07 15:42:26 +0800

    Update manage-date.php

--- manage-date.php
+++ manage-date.php
@@ -29,15 +29,13 @@ if (isset($_POST['deletedate'])) {
 
     $sql = "DELETE FROM xdate WHERE id = '$deletedate'";
 
-    if (mysqli_query($conn,$sql)) {
-        //echo '<script>alert("Success delete closed data");</script>';
-
-        echo "<script>
+    if (mysqli_query($conn,$sql)) {?>
+        <script>
             $(window).load(function(){
                 $('#successmodal').modal('show');
             });
-            </script>";
-    }
+        </script>";
+    <?php }
 }
 
 $result=mysqli_query($conn, "SELECT * FROM xdate WHERE datedisable != ''");
@@ -221,11 +219,6 @@ $user=mysqli_fetch_all($result, MYSQLI_ASSOC);
                 return true;
             }
         }
-    });
-</script>
-<script>
-    $(window).load(function(){
-        $('#successmodal').modal('show');
     });
 </script>
 
