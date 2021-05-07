03e43d76b9bbe118b1d9120fa4a9c78bc71f20e5
heavenluv <weipj128@gmail.com>
2021-05-07 16:43:41 +0800

    chatting area configuration

--- chatting/js/chat.js
+++ chatting/js/chat.js
@@ -41,7 +41,7 @@ ChatBubbleBox.onmouseenter = () => {
 
 sendBtn.onclick = () => {
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/insert-chat.php", true);
+    xhr.open("POST", "insert-chat.php", true);
     xhr.onload = () => {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status === 200) {
@@ -64,7 +64,7 @@ ChatBubbleBox.onmouseleave = () => {
 
 setInterval(() => {
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "./php/get-chat.php", true);
+  xhr.open("POST", "get-chat.php", true);
   xhr.onload = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
