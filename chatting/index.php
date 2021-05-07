c2f6dbbd90f841acd8b6e5c34a3d2af66fbce3bd
heavenluv <weipj128@gmail.com>
2021-05-07 15:24:10 +0800

    chatting area configuration

--- chatting/index.php
+++ chatting/index.php
@@ -188,7 +188,6 @@ Website: http://emilcarlsson.se/
 		<div class="content">
 			<?php
 			$target_id = mysqli_real_escape_string($conn, $_SESSION["target_chat_user"]);
-			console_log($_SESSION["target_chat_user"]);
 			$targetuser = mysqli_query($conn, "SELECT * FROM user WHERE user_id = '$target_id'");
 			if (mysqli_num_rows($targetuser) > 0) {
 				$row = mysqli_fetch_assoc($targetuser);
@@ -327,7 +326,11 @@ Website: http://emilcarlsson.se/
 			method: "POST",
 			data: {
 				"target_userid": target_userid
-			}
+			},
+			success: function(result) {
+				console.log("Obtain function runs here!");
+				console.log(result);
+			},
 		})
 
 
--- chatting/obtainTargetUserID.php
+++ chatting/obtainTargetUserID.php
@@ -2,4 +2,5 @@
     session_start();
     include "../dbconnect.php";
     $_SESSION["target_chat_user"] = $_POST['target_userid'];
+    echo $_SESSION["target_chat_user"];
 ?>
