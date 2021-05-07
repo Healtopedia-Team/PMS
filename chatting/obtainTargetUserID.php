a0af311e16d7610d3c85562b9c32eb5e86c110b8
heavenluv <weipj128@gmail.com>
2021-05-07 15:15:53 +0800

    chatting area configuration

--- chatting/obtainTargetUserID.php
+++ chatting/obtainTargetUserID.php
@@ -1,4 +1,5 @@
 <?php
- $_SESSION["target_chat_user"] = $_POST['target_userid'];
- echo($_SESSION["target_chat_user"]);
+    session_start();
+    include "../dbconnect.php";
+    $_SESSION["target_chat_user"] = $_POST['target_userid'];
 ?>
