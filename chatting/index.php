5ba53cfe05579e26af9125ab4affffca0019b4d1
heavenluv <weipj128@gmail.com>
2021-05-10 15:38:41 +0800

    configure the file upload function

--- chatting/index.php
+++ chatting/index.php
@@ -306,7 +306,7 @@ Website: http://emilcarlsson.se/
 						const ChatBubbleBox = document.querySelector(".content .messages");
 
 						function scrollToBottom() { //Automatically scroll to the bottom of page as keep showing the latest messages
-							ChatBubbleBox.scrollTop = document.body.scrollHeight;
+							ChatBubbleBox.scrollTop = ChatBubbleBox.scrollHeight;
 						}
 
 						function refreshChatRoom() { //Refresh the chatroom while send message or select the user to chat to
