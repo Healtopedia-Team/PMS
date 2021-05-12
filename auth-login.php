d4fd8cf58a4fb1844e58dc76e48153a882132c0e
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-12 16:05:28 +0800

    Update auth-login.php

--- auth-login.php
+++ auth-login.php
@@ -73,7 +73,7 @@
             </div>
             <div class="col-lg-7 d-none d-lg-block">
                 <div id="auth-right">
-                    <img src="doc.jpg" class="card-img-top img-fluid" alt="singleminded">
+                    <img src="doc.jpg" class="card-img-top img-fluid" alt="singleminded" style="height: 100px;width: 100px;">
                 </div>
             </div>
         </div>
