c8a24313895f57ad314757f41251bf3cca5ebe94
hajarrafie <43933592+jajatiha@users.noreply.github.com>
2021-05-18 13:05:53 +0800

    add style

--- po.php
+++ po.php
@@ -11,7 +11,7 @@
     integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
     crossorigin="anonymous"
   />
-  <link rel="stylesheet" href="styles.css" />
+  <link rel="stylesheet" href="report-style.css" />
   <head>
     <meta charset="UTF-8" />
     <script src="script.js"></script>
