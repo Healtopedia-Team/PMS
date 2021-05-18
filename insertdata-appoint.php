ce9976ec2cfbecf5b90bed1aa25ca199f888fdad
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 10:49:34 +0800

    Update insertdata-appoint.php

--- insertdata-appoint.php
+++ insertdata-appoint.php
@@ -26,8 +26,8 @@
                         $sql = " INSERT INTO appointwoo SET order_id = '$orderid', appoint_id = '$appointid', statusapp = '$statusapp', start_appoint = '$startappoint', end_appoint = '$endappoint', prod_id = '$prodid'";
 
                         if (mysqli_query($conn, $sql)) {
-                            $start = date('Y-m-d H:i'$row['start']);
-                            $end = date('Y-m-d H:i'$row['end']);
+                            $start = date('Y-m-d H:i',$row['start']);
+                            $end = date('Y-m-d H:i',$row['end']);
                             $sql2 = "INSERT INTO calendar SET event_title = '$packagename', start_event = '$start', end_event = '$end'";
 
                             mysqli_query($conn, $sql2);
