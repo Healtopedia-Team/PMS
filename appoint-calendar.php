fbf8e6dfd7763cd40be4c1f84c337ed6ca5efb6a
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 18:19:57 +0800

    Update appoint-calendar.php

--- appoint-calendar.php
+++ appoint-calendar.php
@@ -10,6 +10,9 @@ if (!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true) {
 
 $sql = mysqli_query($conn, "SELECT * FROM appointwoo");
 $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+$sql2 = mysqli_query($conn, "SELECT * FROM orderwoo");
+$result2 = mysqli_fetch_all($sql2, MYSQLI_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -69,6 +72,8 @@ $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
                                     <div id="calendar"></div>
                                 </div>
                                 <?php foreach ($result as $row) { ?>
+                                    <?php foreach ($result2 as $row2) { ?>
+                                        <?php if ($row['appoint_id'] == $row2['cust_id']) { ?>
                                 <div class="modal fade" id="detailinfo<?php echo $row['order_id'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable" role="document">
                                         <div class="modal-content">
@@ -79,11 +84,7 @@ $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
                                                 </button>
                                             </div>
                                             <div class="modal-body">
-                                                <p>
-                                                    Start Time : <?php echo date('Y-m-d H:i',$row['start_appoint']);?>
-                                                    <br>
-                                                    End Time : <?php echo date('Y-m-d H:i',$row['end_appoint']);?>
-                                                </p>
+                                                <b>Name : </b><?php echo $row2['firstname'];?> <?php echo $row2['lastname'];?>
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
@@ -98,7 +99,7 @@ $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
                                         </div>
                                     </div>
                                 </div>
-                                <?php } ?>
+                                <?php } } } ?>
                             </div>
                         </div>
                     </div>
