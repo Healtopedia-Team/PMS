1623a25c9dc89fa984906be0f4acba025c4654c8
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 17:33:25 +0800

    Update insertdata-product.php

--- insertdata-product.php
+++ insertdata-product.php
@@ -8,28 +8,6 @@
     $sql = mysqli_query($conn, "SELECT appoint_id, prod_id FROM appointwoo ORDER BY order_id DESC");
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
 
-    foreach ($result as $row) {
-        foreach ($data2 as $row2) {
-            if ($row['prod_id'] == $row2['id']) {
-                $prodid = $row['prod_id'];
-                $name = $row2['name'];
-                $price = $row2['price'];
-                $appointid = $row['appoint_id'];
-
-                $sql2 = mysqli_query($conn, "SELECT COUNT(appoint_id) as Total FROM packagewoo WHERE appoint_id = '$appointid'");
-                $result2 = mysqli_fetch_all($sql2, MYSQLI_ASSOC);
-
-                foreach ($result2 as $key) {
-                    if ($key['Total'] < 1) {
-                        $query = "INSERT INTO packagewoo SET package_id = '$prodid', package_name = '$name', package_price = '$price', appoint_id = '$appointid'";
-                        mysqli_query($conn, $query);
-                    }
-                }
-
-            }
-        }
-    }
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,23 +18,17 @@
     <table>
         <thead>
             <th>PRODUCT ID</th>
-            <th>APPOINT ID</th>
             <th>PRODUCT NAME</th>
             <th>PRICE</th>
         </thead>
         <tbody>
-            <?php foreach ($result as $row) {
-                foreach ($data2 as $row2) {
-                    if ($row['prod_id'] == $row2['id']) { ?>
-                        <tr>
-                            <td><?php echo $row['prod_id'];?></td>
-                            <td><?php echo $row['appoint_id'];?></td>
-                            <td><?php echo $row2['name'];?></td>
-                            <td><?php echo $row2['price'];?></td>
-                        </tr>
-                    <?php }
-                }
-            }?>
+            <?php foreach ($data2 as $row2) { ?>
+                <tr>
+                    <td><?php echo $row['id'];?></td>
+                    <td><?php echo $row['name'];?></td>
+                    <td><?php echo $row['price'];?></td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
 </body>
