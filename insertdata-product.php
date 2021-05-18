535c24b8c4f0d66352ad1fe280e7b18de8ee52a3
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 15:19:20 +0800

    Update insertdata-product.php

--- insertdata-product.php
+++ insertdata-product.php
@@ -11,11 +11,11 @@
     foreach ($result as $row) {
         foreach ($data2 as $row2) {
             if ($row2['id'] == $row['prod_id']) {
-                echo $prodid = $row['prod_id'];
-                echo $prodname = $row2['name'];
-                echo $prodprice = $row2['price'];
+                $prodid = $row['prod_id'];
+                $prodname = $row2['name'];
+                $prodprice = $row2['price'];
 
-                $sql2 = "INSERT INTO packagewoo SET package_id = '$prodid', package_name = '$prodname', package_price = 'prodprice'";
+                $sql2 = "INSERT INTO packagewoo SET package_id = '$prodid', package_name = '$prodname', package_price = '$prodprice'";
                 mysqli_query($conn, $sql2);
 
                 /*$query=mysqli_query($conn, "SELECT COUNT(package_id) as Total FROM packagewoo WHERE package_id = '$prodid'");
