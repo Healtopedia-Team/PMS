51ea289d5c10039521fcff665364f68e681c0e17
hajarrafie <43933592+jajatiha@users.noreply.github.com>
2021-05-18 11:17:14 +0800

    change dompdf structure

--- report-po.php
+++ report-po.php
@@ -9,9 +9,11 @@ use Dompdf\Dompdf;
   $dompdf = new Dompdf();
  
 
+
   //to put other html file
   $html = file_get_contents('https://pms.healtopedia.com/po.php');
-  $dompdf->setBasePath(realpath(''));
+  $html .= '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"/>';
+
 
   $dompdf->loadHtml($html);
 
@@ -22,6 +24,6 @@ use Dompdf\Dompdf;
   $dompdf->render();
 
   // Output the generated PDF (1 = download and 0 = preview)
-  $dompdf->stream("Webslesson", array("Attachment"=>0));
+  $dompdf->stream("Purchase Order", array("Attachment"=>0));
 
 ?>
