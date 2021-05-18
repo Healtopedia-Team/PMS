226a0dee2bb779cf01dd61f47295d1c29f227d2c
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 17:55:31 +0800

    Update insertdata-product.php

--- insertdata-product.php
+++ insertdata-product.php
@@ -1,10 +1,25 @@
 <?php
         
-    $data2 = file_get_contents('https://pms.healtopedia.com/productjson.php');
-    $data2 = json_decode($data2, true);
+    $data = file_get_contents('https://pms.healtopedia.com/productjson.php');
+    $data = json_decode($data, true);
 
     $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
-        
+
+    foreach ($data as $row) {
+        $id = $row['id'];
+        $name = $row['name'];
+        $price = $row['price'];
+
+        $sql = mysqli_query($conn, "SELECT COUNT(package_id) as total FROM packagewoo WHERE package_id = '$id'");
+        $result = mysqli_fetch_all($sql2, MYSQLI_ASSOC);
+
+        foreach ($result as $row2) {
+            if ($row2['total'] < 1) {
+                $sql2 = "INSERT INTO packagewoo SET package_id = '$id', package_name = '$name', package_price = '$price'";
+                mysqli_query($conn, $sql2);
+            }
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,27 +34,13 @@
             <th>PRICE</th>
         </thead>
         <tbody>
-            <?php foreach ($data2 as $row) { ?>
+            <?php foreach ($data as $row) { ?>
                 <tr>
-                    <td><?php echo $id = $row['id'];?></td>
-                    <td><?php echo $name = $row['name'];?></td>
-                    <td><?php echo $price = $row['price'];?></td>
+                    <td><?php echo $row['id'];?></td>
+                    <td><?php echo $row['name'];?></td>
+                    <td><?php echo $row['price'];?></td>
                 </tr>
-            <?php
-                $sql = mysqli_query($conn, "SELECT COUNT(package_id) as total FROM packagewoo WHERE package_id = '$id'");
-                $result = mysqli_fetch_all($sql2, MYSQLI_ASSOC);
-
-                foreach ($result as $row2) {
-                    $id = $row['id'];
-                    $name = $row['name'];
-                    $price = $row['price'];
-                    
-                    if ($row2['total'] < 1) {
-                        $sql2 = "INSERT INTO packagewoo SET package_id = '$id', package_name = '$name', package_price = '$price'";
-                        mysqli_query($conn, $sql2);
-                    }
-                }
-            } ?>
+            <?php } ?>
         </tbody>
     </table>
 </body>
