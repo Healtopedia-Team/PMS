706e786b8c6f00907af8b0b7b7ee4cf61206d5c2
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 15:56:51 +0800

    Update insertdata-product.php

--- insertdata-product.php
+++ insertdata-product.php
@@ -8,26 +8,6 @@
     $sql = mysqli_query($conn, "SELECT prod_id FROM appointwoo ORDER BY order_id DESC");
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
 
-    foreach ($result as $row) {
-        foreach ($data2 as $row2) {
-            if ($row['prod_id'] == $row2['id']) {
-                $prodid = $row['prod_id'];
-                $prodname = $row2['name'];
-                $prodprice = $row2['price'];
-
-                /*$query=mysqli_query($conn, "SELECT COUNT(package_id) as Total FROM packagewoo WHERE package_id = '$prodid'");
-                $user=mysqli_fetch_all($query, MYSQLI_ASSOC);
-
-                foreach ($user as $key) {
-                    if ($key['Total'] < 1) {
-                        $sql2 = "INSERT INTO packagewoo SET package_id = '$prodid', package_name = '$prodname', package_price = '$prodprice'";
-
-                        mysqli_query($conn, $sql2);
-                    }
-                }*/
-            }
-        }
-    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -47,7 +27,7 @@
                     if ($row['prod_id'] == $row2['id']) { ?>
                         <tr>
                             <td><?php echo $row['prod_id'];?></td>
-                            <td><?php echo $$row2['name'];?></td>
+                            <td><?php echo $row2['name'];?></td>
                             <td><?php echo $row2['price'];?></td>
                         </tr>
                     <?php }
