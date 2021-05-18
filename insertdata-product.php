f7a1271d308fd1bb151552266dd5dd9bcd7ce7e3
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 16:49:38 +0800

    Update insertdata-product.php

--- insertdata-product.php
+++ insertdata-product.php
@@ -5,8 +5,30 @@
 
     $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
         
-    $sql = mysqli_query($conn, "SELECT order_id, prod_id FROM appointwoo ORDER BY order_id DESC");
+    $sql = mysqli_query($conn, "SELECT appoint_id, prod_id FROM appointwoo ORDER BY order_id DESC");
     $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+    foreach ($result as $row) {
+        foreach ($data2 as $row2) {
+            if ($row['prod_id'] == $row2['id']) {
+                $prodid = $row['prod_id'];
+                $name = $row2['name'];
+                $price = $row2['price'];
+                $appointid = $row['appoint_id'];
+
+                $sql2 = mysqli_query($conn, "SELECT COUNT(appoint_id) as Total WHERE appoint_id = '$appointid'");
+                $result2 = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+                foreach ($result2 as $key) {
+                    if ($key['Total'] < 1) {
+                        $query = "INSERT INTO packagewoo SET package_id = '$prodid', package_name = '$name', package_price = '$price', appoint_id = '$appointid'";
+                        mysqli_query($conn, $query);
+                    }
+                }
+
+            }
+        }
+    }
 
 ?>
 <!DOCTYPE html>
@@ -18,7 +40,7 @@
     <table>
         <thead>
             <th>PRODUCT ID</th>
-            <th>ORDER ID</th>
+            <th>APPOINT ID</th>
             <th>PRODUCT NAME</th>
             <th>PRICE</th>
         </thead>
@@ -28,7 +50,7 @@
                     if ($row['prod_id'] == $row2['id']) { ?>
                         <tr>
                             <td><?php echo $row['prod_id'];?></td>
-                            <td><?php echo $row['order_id'];?></td>
+                            <td><?php echo $row['appoint_id'];?></td>
                             <td><?php echo $row2['name'];?></td>
                             <td><?php echo $row2['price'];?></td>
                         </tr>
