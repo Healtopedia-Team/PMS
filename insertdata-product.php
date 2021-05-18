b004bc888f407e58c550a74e737cccc23fdd4c2b
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-18 15:30:03 +0800

    Update insertdata-product.php

--- insertdata-product.php
+++ insertdata-product.php
@@ -15,16 +15,20 @@
                 $prodname = $row2['name'];
                 $prodprice = $row2['price'];
 
-                $query=mysqli_query($conn, "SELECT COUNT(package_id) as Total FROM packagewoo WHERE package_id = '$prodid'");
+                $sql2 = "INSERT INTO packagewoo SET package_id = '$prodid', package_name = '$prodname', package_price = '$prodprice'";
+
+                mysqli_query($conn, $sql2);
+
+                /*$query=mysqli_query($conn, "SELECT COUNT(package_id) as Total FROM packagewoo WHERE package_id = '$prodid'");
                 $user=mysqli_fetch_all($query, MYSQLI_ASSOC);
 
                 foreach ($user as $key) {
-                    if ($key['Total'] < 2) {
+                    if ($key['Total'] < 1) {
                         $sql2 = "INSERT INTO packagewoo SET package_id = '$prodid', package_name = '$prodname', package_price = '$prodprice'";
 
                         mysqli_query($conn, $sql2);
                     }
-                }
+                }*/
             }
         }
     }
