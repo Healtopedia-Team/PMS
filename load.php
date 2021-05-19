c0e4d63db12e60e7e8922ef9d7fa86693a1ff423
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-19 11:42:15 +0800

    Update load.php

--- load.php
+++ load.php
@@ -17,8 +17,8 @@ foreach($result as $row)
     $data[] = array(
         'id'   => $row["appoint_id"],
         'title'   => $row["hosp_name"],
-        'start'   => date('Y-m-d H:i',$row['start_appoint']),
-        'end'   => date('Y-m-d H:i',$row['end_appoint'])
+        'start'   => date('Y-m-d H:i',$row['start_appoint']-28800),
+        'end'   => date('Y-m-d H:i',$row['end_appoint']-28800)
     );
 }
 
