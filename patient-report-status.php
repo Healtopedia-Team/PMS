ae90036f86f7e683ce562f053f6024f796fac6c9
heavenluv <weipj128@gmail.com>
2021-05-19 17:43:53 +0800

    pms report css/html

--- patient-report-status.php
+++ patient-report-status.php
@@ -172,7 +172,7 @@ $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
                                         <h6 class="font-bold">Daily Revenue</h6>
                                     </div>
                                     <div class="row px-3 py-1">
-                                        <table id="dtVerticalScrollExample" class="table table-striped table-bordered table-sm" style="font-size: 0.9rem;">
+                                        <table id="dtVerticalScrollExample" cellspacing="0" class="table table-striped table-bordered table-sm" style="font-size: 0.9rem;">
                                             <thead class="thead-light">
                                                 <tr>
                                                     <th scope="col">Date</th>
@@ -252,7 +252,7 @@ $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
             <script>
                 $(document).ready(function() {
                     $('#dtVerticalScrollExample').DataTable({
-                        "scrollY": "50vh",
+                        "scrollY": "30vh",
                         "scrollCollapse": true,
                     });
                     $('.dataTables_length').addClass('bs-select');
