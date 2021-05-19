e66b8f8c8bebe31fd46f36ddaecbd2c9fa475424
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-19 14:51:02 +0800

    Update insertdata-package.php

--- insertdata-package.php
+++ insertdata-package.php
@@ -6,6 +6,7 @@ $product = json_decode($product, true);
 foreach ($product as $row) {
     $prodid = $row['id'];
     $name = $row['name'];
+    echo $row['name'];
 
     $validate = mysqli_query($conn, "SELECT COUNT(package_id) as Total FROM packagewoo WHERE package_id = '$prodid'");
     $validate = mysqli_fetch_all($validate, MYSQLI_ASSOC);
@@ -18,10 +19,10 @@ foreach ($product as $row) {
                 $sql2 = "UPDATE packagewoo SET package_name = '$name' WHERE package_id = '$prodid'";
                 mysqli_query($conn, $sql2);
             }
-        }else{
-            $sql3 = "UPDATE packagewoo SET package_name = '$name' WHERE package_id = '$prodid'";
-            mysqli_query($conn, $sql3);
         }
+
+        $sql3 = "UPDATE packagewoo SET package_name = '$name' WHERE package_id = '$prodid'";
+        mysqli_query($conn, $sql3);
     }
 }
 
