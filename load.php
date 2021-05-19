b893688c627eac73aa735e3f52cf110f3765920f
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-19 12:31:22 +0800

    Update load.php

--- load.php
+++ load.php
@@ -1,27 +1,14 @@
 <?php
-//load.php
-$connect = new PDO('mysql:host=localhost;dbname=db_pms', 'myhealtopedia', 'Healit20.');
+$connect = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
 
 $data = array();
 
-$query = "SELECT * FROM appointwoo WHERE statusapp = 'complete' OR statusapp = 'paid' ORDER BY order_id DESC";
+$query = mysqli_query($connect, "SELECT * FROM appointwoo WHERE statusapp = 'complete' OR statusapp = 'paid' ORDER BY order_id DESC");
+$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-$statement = $connect->prepare($query);
-
-$statement->execute();
-
-$result = $statement->fetchAll();
-
-foreach($result as $row)
-{
-    $data[] = array(
-        'id'   => $row["appoint_id"],
-        'title'   => $row["hosp_name"],
-        'start'   => date('Y-m-d H:i',$row['start_appoint']-28800),
-        'end'   => date('Y-m-d H:i',$row['end_appoint']-28800)
-    );
+foreach($result as $row) {
+    $data['id','title','start','end'] = array($row['appoint_id'], $row['hosp_name'], date('Y-m-d H:i',$row['start_appoint']-28800), date('Y-m-d H:i',$row['end_appoint']-28800));
 }
 
 echo json_encode($data);
-
 ?>
