a341e598cbc8a0bc8ede985aa854fe5d8d03f13b
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-19 14:19:59 +0800

    Update insertdata-package.php

--- insertdata-package.php
+++ insertdata-package.php
@@ -3,9 +3,6 @@
 $product = file_get_contents('https://pms.healtopedia.com/productjson.php');
 $product = json_decode($product, true);
 
-$validate = mysqli_query($conn, "SELECT  FROM packagewoo");
-$validate = mysqli_fetch_all($validate, MYSQLI_ASSOC);
-
 foreach ($product as $row) {
     $prodid = $row['id'];
 
@@ -15,7 +12,10 @@ foreach ($product as $row) {
     foreach ($validate as $row2) {
         if ($row2['Total'] < 1) {
             $sql = "INSERT INTO packagewoo SET package_id = '$prodid'";
-            mysqli_query($conn, $sql);
+            
+            if (mysqli_query($conn, $sql)){
+                echo $prodid;
+            }
         }
     }
 }
