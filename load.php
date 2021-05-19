9a05dacc11df540d2da98aaf10f8c68d8a6fefb0
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-19 11:04:28 +0800

    Update load.php

--- load.php
+++ load.php
@@ -4,7 +4,7 @@ $connect = new PDO('mysql:host=localhost;dbname=db_pms', 'myhealtopedia', 'Heali
 
 $data = array();
 
-$query = "SELECT * FROM appointwoo ORDER BY order_id DESC";
+$query = "SELECT * FROM orderwoo ORDER BY order_id DESC";
 
 $statement = $connect->prepare($query);
 
@@ -15,10 +15,10 @@ $result = $statement->fetchAll();
 foreach($result as $row)
 {
     $data[] = array(
-        'id'   => $row["appoint_id"],
-        'title'   => $row["hosp_name"],
-        'start'   => date('Y-m-d H:i',$row['start_appoint']),
-        'end'   => date('Y-m-d H:i',$row['end_appoint'])
+        'id'   => $row["order_id"]
+        //'title'   => $row["hosp_name"],
+        //'start'   => date('Y-m-d H:i',$row['start_appoint']),
+        //'end'   => date('Y-m-d H:i',$row['end_appoint'])
     );
 }
 
