0c6b0a1aabdfebd8fad8759f96f5efa46a1f9aff
heavenluv <weipj128@gmail.com>
2021-05-19 16:33:37 +0800

    pms report css/html

---
+++ patient-report-status.php
@@ -0,0 +1,144 @@
+<?php
+
+$conn = mysqli_connect("localhost", "myhealtopedia", "Healit20.", "db_pms");
+
+$directoryURI = $_SERVER['REQUEST_URI'];
+$path = parse_url($directoryURI, PHP_URL_PATH);
+$components = explode('/', $path);
+$your_variable = basename($_SERVER['PHP_SELF'], ".php");
+
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["name"]) || $_SESSION["loggedin"] !== true) {
+    header("location: auth-login.php");
+    exit;
+}
+$result = mysqli_query($conn, "SELECT * FROM requestappoint WHERE req_status = 'completed' ORDER BY request_id");
+$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient Report - Healtopedia Digital</title>
+
+    <link rel="stylesheet" href="assets/vendors/choices.js/choices.min.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <link rel="stylesheet" href="assets/vendors/simple-datatables/style.css">
+
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.js"></script>
+</head>
+
+<body>
+    <div id="app">
+        <?php include 'sidebar.php'; ?>
+        <div id="main" style="margin-top: -50px;">
+            <div class="page-heading">
+                <div class="page-title">
+                    <div class="row">
+                        <div class="col-12 col-md-6 order-md-1 order-last">
+                            <h3>Patient Report</h3>
+                        </div>
+                        <div class="col-12 col-md-6 order-md-2 order-first">
+                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Patient Report</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <section class="section">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-body px-2 py-2-3">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h6 class="text-muted font-semibold">Gross Revenue</h6>
+                                            <h6 class="font-extrabold mb-0">
+                                            </h6>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="stats-icon purple">
+                                                <i class="iconly-boldShow"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-body px-2 py-2-3">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h6 class="text-muted font-semibold">Refunds</h6>
+                                            <h6 class="font-extrabold mb-0">
+                                            </h6>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="stats-icon purple">
+                                                <i class="iconly-boldShow"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-body px-2 py-2-3">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h6 class="text-muted font-semibold">Net Revenue</h6>
+                                            <h6 class="font-extrabold mb-0">
+                                            </h6>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="stats-icon purple">
+                                                <i class="iconly-boldShow"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="card">
+                                <div class="card-body px-2 py-2-3">
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card">
+                                <div class="card-body px-2 py-2-3">
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <?php include 'request-appointment-footer.php'; ?>
