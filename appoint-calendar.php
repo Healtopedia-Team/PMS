d651956987fd8678b4646890ee1bb740eb3de712
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-19 11:26:22 +0800

    Update appoint-calendar.php

--- appoint-calendar.php
+++ appoint-calendar.php
@@ -72,8 +72,6 @@ $result2 = mysqli_fetch_all($sql2, MYSQLI_ASSOC);
                                     <div id="calendar"></div>
                                 </div>
                                 <?php foreach ($result as $row) { ?>
-                                    <?php foreach ($result2 as $row2) { ?>
-                                        <?php if ($row['appoint_id'] == $row2['cust_id']) { ?>
                                 <div class="modal fade" id="detailinfo<?php echo $row['order_id'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable" role="document">
                                         <div class="modal-content">
@@ -96,7 +94,7 @@ $result2 = mysqli_fetch_all($sql2, MYSQLI_ASSOC);
                                         </div>
                                     </div>
                                 </div>
-                                <?php } } } ?>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
