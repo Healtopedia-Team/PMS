4e9d0ebe3147b882efcb0ddd4e0c77ba7289e926
heavenluv <weipj128@gmail.com>
2021-05-20 13:02:36 +0800

    configure the file upload function

--- index.php
+++ index.php
@@ -370,10 +370,57 @@ $hospital_list = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
     <script src="assets/vendors/apexcharts/apexcharts.js"></script>
     <!-- <script src="assets/js/pages/dashboard.js"></script> -->
-    
 
     <script src="assets/js/main.js"></script>
-    <script src="app_chart.js"></script>
+    <script>
+        $(document).ready(function() {
+
+
+            function load_chart() {
+                var month_cnt = []
+                var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+                $.getJSON('month_app_chart.php', function(jsonObject) {
+                    let i = 0
+                    for (let x in months) {
+                        if (i < 12) {
+                            month_cnt.push(parseInt(jsonObject[x]))
+                        }
+                        i += 1;
+                    }
+                    //
+                })
+                return month_cnt
+            }
+            //console.log(month_cnt)
+
+            var optionsProfileVisit = {
+                annotations: {
+                    position: 'back'
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                chart: {
+                    type: 'bar',
+                    height: 300
+                },
+                fill: {
+                    opacity: 1
+                },
+                plotOptions: {},
+                series: [{
+                    name: 'Appointments',
+                    data: setTimeout(load_chart, 500)
+                }],
+                colors: '#435ebe',
+                xaxis: {
+                    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                },
+            }
+            var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
+            chartProfileVisit.render();
+        });​
+    </script>
 </body>
 
 </html>
