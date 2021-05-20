2585fdc6f68a155bef4b8244b560503ecc0557f6
heavenluv <weipj128@gmail.com>
2021-05-20 13:49:48 +0800

    configure the file upload function

--- index.php
+++ index.php
@@ -374,62 +374,77 @@ $hospital_list = mysqli_fetch_all($result2, MYSQLI_ASSOC);
     <script>
         function load_chart() {
             var month_cnt = []
-            var optionsProfileVisit = {
-                annotations: {
-                    position: 'back'
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                chart: {
-                    type: 'bar',
-                    height: 300,
-                    events: {
-                        beforeMount: function(chartContext, config) {
-                            var months = [
-                                'Jan',
-                                'Feb',
-                                'Mar',
-                                'Apr',
-                                'May',
-                                'Jun',
-                                'Jul',
-                                'Aug',
-                                'Sep',
-                                'Oct',
-                                'Nov',
-                                'Dec'
-                            ]
-                            $.getJSON('month_app_chart.php', function(jsonObject) {
-                                let i = 0
-                                for (let x in months) {
-                                    if (i < 12) {
-                                        month_cnt.push(parseInt(jsonObject[x]))
-                                    }
-                                    i += 1
-                                }
-                                //
-                            })
-                        }
+            var months = [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
+            ]
+            $.getJSON('month_app_chart.php', function(jsonObject) {
+                let i = 0
+                for (let x in months) {
+                    if (i < 12) {
+                        month_cnt.push(parseInt(jsonObject[x]))
                     }
-                },
-                fill: {
-                    opacity: 1
-                },
-                plotOptions: {},
-                series: [{
-                    name: 'Appointments',
-                    data: month_cnt
-                }],
-                colors: '#435ebe',
-                xaxis: {
-                    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                },
-            }
-            var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
-            chartProfileVisit.render();
+                    i += 1
+                }
+                //
+            })
+            return month_cnt
         }
-        setTimeout(load_chart, 500)
+        //console.log(month_cnt)
+
+        var optionsProfileVisit = {
+            annotations: {
+                position: 'back'
+            },
+            dataLabels: {
+                enabled: false
+            },
+            chart: {
+                type: 'bar',
+                height: 300
+            },
+            fill: {
+                opacity: 1
+            },
+            plotOptions: {},
+            series: [{
+                name: 'Appointments',
+                data: load_chart()
+            }],
+            colors: '#435ebe',
+            xaxis: {
+                categories: [
+                    'Jan',
+                    'Feb',
+                    'Mar',
+                    'Apr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Aug',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Dec'
+                ]
+            }
+        }
+        var chartProfileVisit = new ApexCharts(
+            document.querySelector('#chart-profile-visit'),
+            optionsProfileVisit
+        )
+        chartProfileVisit.render()
+        setTimeout(load_chart(), 500)
         //window.onload = load_chart()
         //console.log(month_cnt)
         /*
