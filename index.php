ec6acf8c46a68f2546b3040d8f86fc95d8ff3f77
heavenluv <weipj128@gmail.com>
2021-05-20 13:31:41 +0800

    configure the file upload function

--- index.php
+++ index.php
@@ -373,47 +373,63 @@ $hospital_list = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
     <script src="assets/js/main.js"></script>
     <script>
-        var month_cnt = []
-        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-        $.getJSON('month_app_chart.php', function(jsonObject) {
-            let i = 0
-            for (let x in months) {
-                if (i < 12) {
-                    month_cnt.push(parseInt(jsonObject[x]))
+        function load_chart() {
+            var month_cnt = []
+            var months = [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
+            ]
+            $.getJSON('month_app_chart.php', function(jsonObject) {
+                let i = 0
+                for (let x in months) {
+                    if (i < 12) {
+                        month_cnt.push(parseInt(jsonObject[x]))
+                    }
+                    i += 1
                 }
-                i += 1;
+                //
+            })
+            var optionsProfileVisit = {
+                annotations: {
+                    position: 'back'
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                chart: {
+                    type: 'bar',
+                    height: 300
+                },
+                fill: {
+                    opacity: 1
+                },
+                plotOptions: {},
+                series: [{
+                    name: 'Appointments',
+                    data: month_cnt
+                }],
+                colors: '#435ebe',
+                xaxis: {
+                    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                },
             }
-            //
-        })
-        //console.log(month_cnt)
-
-        var optionsProfileVisit = {
-            annotations: {
-                position: 'back'
-            },
-            dataLabels: {
-                enabled: false
-            },
-            chart: {
-                type: 'bar',
-                height: 300
-            },
-            fill: {
-                opacity: 1
-            },
-            plotOptions: {},
-            series: [{
-                name: 'Appointments',
-                data: month_cnt
-            }],
-            colors: '#435ebe',
-            xaxis: {
-                categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            },
+            var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
+            chartProfileVisit.render();
         }
-        var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
-        chartProfileVisit.render();
+        //console.log(month_cnt)
+        $(document).ready(function() {
+            load_chart()
+        });
     </script>
 </body>
 
