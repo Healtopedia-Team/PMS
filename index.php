7c35759e99820624a28a1123275e5c2d818ce868
heavenluv <weipj128@gmail.com>
2021-05-20 13:33:38 +0800

    configure the file upload function

--- index.php
+++ index.php
@@ -426,10 +426,13 @@ $hospital_list = mysqli_fetch_all($result2, MYSQLI_ASSOC);
             var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
             chartProfileVisit.render();
         }
+        window.onload = load_chart
         //console.log(month_cnt)
+        /*
         $(document).ready(function() {
             load_chart()
         });
+        */
     </script>
 </body>
 
