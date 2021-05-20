9b6495ba2664f0049ac8db504de1215c68658325
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-20 16:21:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -224,6 +224,16 @@ $hospital_list = mysqli_fetch_all($result2, MYSQLI_ASSOC);
                                                                 <?php echo $rows['req_packname']; ?><br>
                                                                 <?php echo $rows['req_apptime']; ?><br>
 
+                                                                <?php
+
+                                                                $status = $rows['req_status'];
+                                                                if ($status == "approved") {
+                                                                ?>
+                                                                    <span class="badge bg-primary">Booked</span>
+
+                                                            </td>
+                                                            <td class="text-bold-500">
+                                                                <button type="button" class="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#inlineForm">Check-In</button>
                                                             </td>
                                                         </tr>
                                                         <?php } ?>
