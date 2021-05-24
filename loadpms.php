d417afcde1b58305ef91f14afac70036b834607d
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-24 11:56:03 +0800

    Update loadpms.php

--- loadpms.php
+++ loadpms.php
@@ -9,11 +9,19 @@ $yesterday = strtotime($date);
 
 $query = "SELECT * FROM requestappoint WHERE req_appdate > '$date' AND (req_status = 'approved' OR req_status = 'postponed') ORDER BY request_id DESC";
 
+$query2 = "SELECT * FROM appointwoo WHERE start_appoint > '$yesterday' AND (statusapp = 'paid' OR statusapp = 'complete') ORDER BY order_id DESC";
+
 $statement = $connect->prepare($query);
+
+$statement2 = $connect->prepare($query2);
 
 $statement->execute();
 
+$statement2->execute();
+
 $result = $statement->fetchAll();
+
+$result2 = $statement2->fetchAll();
 
 foreach($result as $row)
 {
@@ -25,6 +33,18 @@ foreach($result as $row)
     );
 }
 
+foreach($result2 as $row2)
+{
+    $data2[] = array(
+        'id'   => $row2["appoint_id"],
+        'title'   => $row2["hosp_name"],
+        'start'   => date('Y-m-d H:i',$row2['start_appoint']-28800),
+        'end'   => date('Y-m-d H:i',$row2['end_appoint']-28800)
+    );
+}
+
 echo json_encode($data);
+
+echo json_encode($data2);
 
 ?>
