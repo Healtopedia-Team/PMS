dffb1b56f128b5e7ae79743a1c226d9c74ccca2b
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-05-24 11:59:01 +0800

    Update po.php

--- po.php
+++ po.php
@@ -7,7 +7,6 @@ $current_date = $_GET['cur_date'];
 $query = "SELECT a.order_id, a.appoint_id, a.start_appoint, a.hosp_name, b.firstname, b.lastname, c.package_name, c.package_price, DATE(FROM_UNIXTIME(a.start_appoint, '%Y-%m-%d')) AS c_date FROM appointwoo a LEFT JOIN orderwoo b ON a.order_id=b.order_id LEFT JOIN packagewoo c ON a.prod_id=c.package_id WHERE FROM_UNIXTIME(a.end_appoint, '%Y-%m-%d')='$current_date' AND a.hosp_name='$hosp' ";
 $result = mysqli_query($conn, $query);
 $res = mysqli_fetch_all($result, MYSQLI_ASSOC);
-$name = "Mayor";
 //var_dump($result->error);
 ?>
 
@@ -223,7 +222,7 @@ $name = "Mayor";
             <font color="#000000"><?php echo $row['hosp_name'] . " --- " . $row['package_name'] . " --- " . date("(d/m/Y)(h:ia)", $row['start_appoint']) . " (" . $row['firstname'] . " " . $row['lastname'] . ")" ?></font>
           </td>
           <td style="border-top: 1px solid #b7b7b7; border-bottom: 1px solid #b7b7b7; border-left: 1px solid #b7b7b7; border-right: 1px solid #b7b7b7" align="center" valign="middle">
-            <font color="#000000"><?php echo $name; ?></font>
+            <font color="#000000"><?php echo date("d/m/Y", strtotime($current_date)) ?></font>
           </td>
           <td style="border-top: 1px solid #b7b7b7; border-bottom: 1px solid #b7b7b7; border-left: 1px solid #b7b7b7; border-right: 1px solid #b7b7b7" align="right" valign="middle">
             <font color="#000000"><?php echo $row['package_price'] ?></font>
