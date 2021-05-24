c78fbc249b569aaa299b90417f7de0266ac6871b
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-05-24 11:21:40 +0800

    Update po.php

--- po.php
+++ po.php
@@ -4,10 +4,7 @@ session_start();
 $hosp = $_SESSION['hospital'];
 $current_date = $_GET['cur_date'];
 //var_dump($current_date);
-$query = "SELECT a.order_id, a.appoint_id, a.start_appoint, a.hosp_name, b.firstname, b.lastname, c.package_name, c.package_price 
-            , DATE(FROM_UNIXTIME(a.start_appoint, '%Y-%m-%d')) AS c_date
-            FROM appointwoo a LEFT JOIN orderwoo b ON a.order_id=b.order_id LEFT JOIN packagewoo c ON a.prod_id=c.package_id 
-            WHERE FROM_UNIXTIME(a.end_appoint, '%Y-%m-%d')='$current_date' AND a.hosp_name='$hosp' ";
+$query = "SELECT a.order_id, a.appoint_id, a.start_appoint, a.hosp_name, b.firstname, b.lastname, c.package_name, c.package_price, DATE(FROM_UNIXTIME(a.start_appoint, '%Y-%m-%d')) AS c_date FROM appointwoo a LEFT JOIN orderwoo b ON a.order_id=b.order_id LEFT JOIN packagewoo c ON a.prod_id=c.package_id WHERE FROM_UNIXTIME(a.end_appoint, '%Y-%m-%d')='$current_date' AND a.hosp_name='$hosp' ";
 $result = mysqli_query($conn, $query);
 $res = mysqli_fetch_all($result, MYSQLI_ASSOC);
 //var_dump($result->error);
