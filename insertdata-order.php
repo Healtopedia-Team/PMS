5023b6e7f98772e469fb01011ad65310ff7becb0
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-24 18:23:13 +0800

    Update insertdata-order.php

--- insertdata-order.php
+++ insertdata-order.php
@@ -1,73 +1,39 @@
 <?php
-$conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
 
-$data = mysqli_query($conn, "SELECT * FROM appointwoo");
-$result = mysqli_fetch_all($data, MYSQLI_ASSOC);
+    header("refresh: 60");
 
-$data2 = mysqli_query($conn, "SELECT * FROM requestappoint");
-$result2 = mysqli_fetch_all($data2, MYSQLI_ASSOC);
+    $data = file_get_contents('https://pms.healtopedia.com/slot2json.php');
+    $data = json_decode($data, true);
+    $data2 = file_get_contents('https://pms.healtopedia.com/slotjson.php');
+    $data2 = json_decode($data2, true);
 
-foreach ($result as $row) {
-    $appointid = $row['appoint_id'];
-    $hospname = $row['hosp_name'];
-    $startapp = $row['start_appoint'];
-    $endapp = $row['end_appoint'];
-    $statusapp = $row['statusapp'];
+    $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
 
-    $valid = mysqli_query($conn, "SELECT COUNT(*) as Total FROM appointwoo WHERE appoint_id = '$appointid'");
-    $ans = mysqli_fetch_all($valid, MYSQLI_ASSOC);
+    foreach ($data as $row ){
+        if ($row['status'] == "completed" || $row['status'] == "processing") {
 
-    foreach ($ans as $key) {
-        if ($key['Total'] < 1) {
-            $sql = "INSERT INTO calendar SET cal_id = '$appointid', cal_name = '$hospname', cal_start = '$startapp', cal_end = '$endapp', cal_status = '$statusapp'";
-            if(mysqli_query($conn, $sql)){
-                echo "<script>alert('Success');</script>"
+            $firstname = $row['billing']['first_name'];
+            $lastname = $row['billing']['last_name'];
+            $orderid = $row['number'];
+            $status = $row['status'];
+            $orderdate = $row['date_created'];
+
+            foreach ($data2 as $row2) {
+                if ($row2['order_id'] == $row['number']) {
+                    $custid = $row2['id'];
+                }
+            }
+
+            $result=mysqli_query($conn, "SELECT COUNT(cust_id) as Total FROM orderwoo WHERE cust_id = '$custid'");
+            $user=mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+            foreach ($user as $key) {
+                if ($key['Total'] < 1) {
+                    $sql = "INSERT INTO orderwoo SET firstname = '$firstname', lastname = '$lastname', order_id = '$orderid', cust_id = '$custid', status = '$status', order_date = '$orderdate'";
+
+                mysqli_query($conn, $sql);
+                }
             }
         }
     }
-}
-
-/*foreach ($result2 as $row2) {
-    $reqid = $row2['request_id'];
-    $packname = $row2['req_packname'];
-    $appdate = $row2['req_appdate'];
-    $status = $row2['req_status'];
-    if ($row2['req_apptime'] == "09:00AM") {
-        $start = date('H:i', strtotime('09:00:00'));
-    }elseif ($row2['req_apptime'] == "10:00AM") {
-        $start = date('H:i', strtotime('10:00:00'));
-    }elseif ($row2['req_apptime'] == "11:00AM") {
-       $start = date('H:i', strtotime('11:00:00'));
-    }elseif ($row2['req_apptime'] == "12:00PM") {
-        $start = date('H:i', strtotime('12:00:00'));
-    }elseif ($row2['req_apptime'] == "01:00PM") {
-        $start = date('H:i', strtotime('13:00:00'));
-    }elseif ($row2['req_apptime'] == "02:00PM") {
-        $start = date('H:i', strtotime('14:00:00'));
-    }elseif ($row25'req_apptime'] == "03:00PM") {
-        $start = date('H:i', strtotime('15:00:00'));
-    }elseif ($row2['req_apptime'] == "04:00PM") {
-        $start = date('H:i', strtotime('16:00:00'));
-    }elseif ($row2['req_apptime'] == "05:00PM") {
-        $start = date('H:i', strtotime('17:00:00'));
-    }elseif ($row2['req_apptime'] == "06:00PM") {
-        $start = date('H:i', strtotime('18:00:00'));
-    }elseif ($row2['req_apptime'] == "07:00PM") {
-        $start = date('H:i', strtotime('19:00:00'));
-    }elseif ($row2['req_apptime'] == "08:00PM") {
-        $start = date('H:i', strtotime('20:00:00'));
-    }elseif ($row2['req_apptime'] == "09:00PM") {
-        $start = date('H:i', strtotime('21:00:00'));
-    }
-
-    $valid2 = mysqli_query($conn, "SELECT COUNT(*) as Total FROM requestappoint HWERE request_id = '$reqid'");
-    $ans2 = mysqli_fetch_all($valid2, MYSQLI_ASSOC);
-
-    foreach ($ans2 as $key2) {
-        if ($key2['Total'] < 1) {
-            $sql2 = "INSERT INTO calendar SET cal_id = '$reqid', cal_name = '$packname', cal_start = '$appdate $start', cal_status = '$status'";
-            mysqli_query($conn, $sql2);
-        }
-    }
-}*/
 ?>
