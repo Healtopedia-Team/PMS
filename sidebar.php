c84f9befb407cd35e08198831f36f49bd15f34e4
heavenluv <weipj128@gmail.com>
2021-05-25 12:36:42 +0800

    notification debugging

--- sidebar.php
+++ sidebar.php
@@ -237,8 +237,8 @@ $role = $_SESSION['role'];
                             </span>
                         </a>
                         <!-- Here not done -->
-                        <ul class="dropdown-menu dropdown-menu-end w-80" aria-labelledby="dropdownMenuButton" style="
-                         overflow-y:scroll; right:0 !important;">
+                        <ul class="dropdown-menu dropdown-menu-end w-100" aria-labelledby="dropdownMenuButton" style="
+                        width: calc(0.2*100vw + 100px) !important; max-height: 400px !important; overflow-y:scroll; right:0 !important;">
                             <li style="word-wrap: break-word;">
                                 <h6 class=" dropdown-header">Notifications</h6>
                             </li>
@@ -267,7 +267,7 @@ $role = $_SESSION['role'];
                                                     -->
                                                     <?php echo $rows[5] ?>
                                                 </i></small><br />
-                                            <?php echo "You just successfully reserved an appointment on {$rows[6]}" ?>
+                                            <?php echo "You just successfully reserved an" ?> <br /> <?php echo "appointment on {$rows[6]}" ?>
                                         </a>
                                     </li>
                                 <?php
