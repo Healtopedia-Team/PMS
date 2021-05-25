998c0753c6edb0f8e23108d32bc1555db7cd43c5
heavenluv <weipj128@gmail.com>
2021-05-25 11:20:22 +0800

    report pdf debugging

--- report-po.php
+++ report-po.php
@@ -17,11 +17,10 @@ define('DOMPDF_ENABLE_PHP', true);
   //ob_get_clean();
 
   //to put other html file
-  /*
   $html = file_get_contents('https://pms.healtopedia.com/po.php?cur_date='.$cur_date);
   $dompdf->setBasePath(realpath('report-style.css'));
 
-  $dompdf->loadHtml($html);
+  $dompdf->load_html_file('https://pms.healtopedia.com/po.php?cur_date=' . $cur_date);
 
   // (Optional) Setup the paper size and orientation
   $dompdf->setPaper('A4','portrait');
@@ -31,7 +30,7 @@ define('DOMPDF_ENABLE_PHP', true);
 
   // Output the generated PDF (1 = download and 0 = preview)
   $dompdf->stream("Purchase Order", array("Attachment"=>0));
-*/
+/*
   ob_start();
   include 'https://pms.healtopedia.com/po.php?cur_date=' . $cur_date ;
   $dompdf->setBasePath(realpath('report-style.css'));
@@ -45,7 +44,7 @@ define('DOMPDF_ENABLE_PHP', true);
   $dompdf->render();
   //$output = $dompdf->output();
   //file_put_contents('/files/report.pdf', $output);
-  $dompdf->stream("Purchase Order", array("Attachment" => 0));
+  //$dompdf->stream("Purchase Order", array("Attachment" => 0));
 
-
+*/
 ?>
