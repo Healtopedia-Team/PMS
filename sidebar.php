7283ebe2cc4e503c075b5ff09f100539d6670eb2
heavenluv <weipj128@gmail.com>
2021-05-25 12:34:45 +0800

    notification debugging

--- sidebar.php
+++ sidebar.php
@@ -237,8 +237,8 @@ $role = $_SESSION['role'];
                             </span>
                         </a>
                         <!-- Here not done -->
-                        <ul class="dropdown-menu dropdown-menu-end w-100" aria-labelledby="dropdownMenuButton" style="
-                        width: calc(0.2*100vw + 100px) !important; max-height: 400px !important; overflow-y:scroll; right:0 !important;">
+                        <ul class="dropdown-menu dropdown-menu-end w-50" aria-labelledby="dropdownMenuButton" style="
+                         overflow-y:scroll; right:0 !important;">
                             <li style="word-wrap: break-word;">
                                 <h6 class=" dropdown-header">Notifications</h6>
                             </li>
