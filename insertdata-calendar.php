a71b27aae99387c4f6679196131eb800493ee40c
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-25 12:21:19 +0800

    Update insertdata-calendar.php

--- insertdata-calendar.php
+++ insertdata-calendar.php
@@ -34,54 +34,67 @@ foreach ($result2 as $row2) {
     if ($row2['req_apptime'] == "09:00AM")
     {
         $start = $appdate." ".date('H:i', strtotime('09:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('10:00:00'));
     }
     elseif ($row2['req_apptime'] == "10:00AM")
     {
         $start = $appdate." ".date('H:i', strtotime('10:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('11:00:00'));
     }
     elseif ($row2['req_apptime'] == "11:00AM")
     {
         $start = $appdate." ".date('H:i', strtotime('11:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('12:00:00'));
     }
     elseif ($row2['req_apptime'] == "12:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('12:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('13:00:00'));
     }
     elseif ($row2['req_apptime'] == "01:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('13:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('14:00:00'));
     }
     elseif ($row2['req_apptime'] == "02:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('14:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('15:00:00'));
     }
     elseif ($row2['req_apptime'] == "03:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('15:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('16:00:00'));
     }
     elseif ($row2['req_apptime'] == "04:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('16:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('17:00:00'));
     }
     elseif ($row2['req_apptime'] == "05:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('17:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('18:00:00'));
     }
     elseif ($row2['req_apptime'] == "06:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('18:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('19:00:00'));
     }
     elseif ($row2['req_apptime'] == "07:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('19:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('20:00:00'));
     }
     elseif ($row2['req_apptime'] == "08:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('20:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('21:00:00'));
     }
     elseif ($row2['req_apptime'] == "09:00PM")
     {
         $start = $appdate." ".date('H:i', strtotime('21:00:00'));
+        $end = $appdate." ".date('H:i', strtotime('22:00:00'));
     }
 
     $valid2 = mysqli_query($conn, "SELECT COUNT(*) as Total FROM calendar WHERE cal_id = '$reqid'");
@@ -89,7 +102,7 @@ foreach ($result2 as $row2) {
 
     foreach ($ans2 as $key2) {
         if ($key2['Total'] < 1) {
-            $sql2 = "INSERT INTO calendar SET cal_id = '$reqid', cal_name = '$packname', cal_start = '$start', cal_status = '$status'";
+            $sql2 = "INSERT INTO calendar SET cal_id = '$reqid', cal_name = '$packname', cal_start = '$start', cal_end = '$end', cal_status = '$status'";
             mysqli_query($conn, $sql2);
         }
     }
