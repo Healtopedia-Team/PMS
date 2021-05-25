92a5e06632294e4155e4f61ebcb4520dfb71b024
heavenluv <weipj128@gmail.com>
2021-05-25 11:03:54 +0800

    report pdf debugging

--- report-po.php
+++ report-po.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'dompdf/autoload.inc.php';
 $cur_date = $_GET['cur_date'];
+error_reporting(E_ALL);
 
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
@@ -10,13 +11,13 @@ define('DOMPDF_ENABLE_PHP', true);
   // instantiate and use the dompdf class
   $dompdf = new Dompdf();
   //require_once 'https://pms.healtopedia.com/po.php?cur_date='. $cur_date ;
-  ob_start();
-  require('https://pms.healtopedia.com/po.php?cur_date=' . $cur_date);
+  //ob_start();
+  //require('https://pms.healtopedia.com/po.php?cur_date=' . $cur_date);
   $html = ob_get_contents();
-  ob_get_clean();
+  //ob_get_clean();
 
   //to put other html file
-  //$html = file_get_contents('https://pms.healtopedia.com/po.php?cur_date='.$cur_date);
+  $html = file_get_contents('https://pms.healtopedia.com/po.php?cur_date='.$cur_date);
   $dompdf->setBasePath(realpath('report-style.css'));
 
   $dompdf->loadHtml($html);
