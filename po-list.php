b74925d86040c77a2992aea547b7cf22689433f7
heavenluv <weipj128@gmail.com>
2021-05-27 16:41:37 +0800

    sql injection prevention -- patient-report.php

--- po-list.php
+++ po-list.php
@@ -8,12 +8,17 @@
                             AS unique_date, COUNT(*) AS amount
                             FROM `appointwoo`
                             WHERE DATEDIFF(NOW(), FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 
-                            AND hosp_name = '$hosp' 
+                            AND hosp_name = ? 
                             AND statusapp='complete'
                             GROUP BY unique_date
                             ORDER BY unique_date DESC";
-                $result = mysqli_query($conn, $query);
-                $res = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                //$result = mysqli_query($conn, $query);
+                //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+                $result = $conn->prepare($query);
+                $result->bind_param("s", $hosp);
+                $result->execute();
+                $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
                 ?>
 
                 <section class="section">
