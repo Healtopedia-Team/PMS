48e176a89db5514471d3d8c19481dd4961fae8b4
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-05-27 15:53:53 +0800

    Update loadhealtopedia.php

--- loadhealtopedia.php
+++ loadhealtopedia.php
@@ -28,3 +28,33 @@ foreach($result as $row)
 echo json_encode($data);
 
 ?>
+<?php
+//load.php
+$connect = new PDO('mysql:host=localhost;dbname=db_pms', 'myhealtopedia', 'Healit20.');
+
+$data = array();
+
+$date = date('Y-m-d',strtotime("-1 days"));
+$yesterday = strtotime($date);
+
+$query = "SELECT * FROM calendar WHERE cal_start > '2021-05-26' AND (cal_status = 'complete' OR cal_status = 'paid' OR cal_status = 'approved')";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+foreach($result as $row)
+{
+    $data[] = array(
+        'id'   => $row["cal_id"],
+        'title'   => $row["cal_name"],
+        'start'   => $row["cal_start"],
+        'end'   => $row["cal_end"]
+    );
+}
+
+echo json_encode($data);
+
+?>
