6ce9cc9872c47728f8843b4d5524ad7bcc5f88b5
heavenluv <weipj128@gmail.com>
2021-05-27 12:40:33 +0800

    polist debugging

--- report-po.php
+++ report-po.php
@@ -6,6 +6,7 @@ $cur_date = $_GET['cur_date'];
 
 // reference the Dompdf namespace
 ini_set("memory_limit", "800M");
+ini_set("max_execution_time", "800");
 use Dompdf\Dompdf;
 
 define('DOMPDF_ENABLE_PHP', true);
