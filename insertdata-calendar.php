057919b65c64fd3c9231b6a2ca63cee15cad207e
heavenluv <weipj128@gmail.com>
2021-05-27 15:32:00 +0800

    sql injection prevention -- insertdata-calendar.php

--- insertdata-calendar.php
+++ insertdata-calendar.php
@@ -2,12 +2,20 @@
 header("refresh: 60");
 
 $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
-
+/*
 $data = mysqli_query($conn, "SELECT * FROM appointwoo");
 $result = mysqli_fetch_all($data, MYSQLI_ASSOC);
 
 $data2 = mysqli_query($conn, "SELECT * FROM requestappoint");
 $result2 = mysqli_fetch_all($data2, MYSQLI_ASSOC);
+*/
+$data = $conn->prepare("SELECT * FROM appointwoo");
+$data->execute();
+$result = $data->get_result()->fetch_all(MYSQLI_ASSOC);
+
+$data2 = $conn->prepare("SELECT * FROM requestappoint");
+$data2->execute();
+$result2 = $data2->get_result()->fetch_all(MYSQLI_ASSOC);
 
 foreach ($result as $row) {
     $appointid = $row['appoint_id'];
@@ -16,13 +24,21 @@ foreach ($result as $row) {
     $endapp = date('Y-m-d H:i',$row['end_appoint']-28800);
     $statusapp = $row['statusapp'];
 
-    $valid = mysqli_query($conn, "SELECT COUNT(*) as Total FROM calendar WHERE cal_id = '$appointid'");
-    $ans = mysqli_fetch_all($valid, MYSQLI_ASSOC);
+    //$valid = mysqli_query($conn, "SELECT COUNT(*) as Total FROM calendar WHERE cal_id = '$appointid'");
+    //$ans = mysqli_fetch_all($valid, MYSQLI_ASSOC);
+
+    $valid = $conn->prepare("SELECT COUNT(*) as Total FROM calendar WHERE cal_id =? ");
+    $valid->bind_param("i", $appointid);
+    $valid->execute();
+    $ans = $valid->get_result()->fetch_all(MYSQLI_ASSOC);
 
     foreach ($ans as $key) {
         if ($key['Total'] < 1) {
-            $sql = "INSERT INTO calendar SET cal_id = '$appointid', cal_name = '$hospname', cal_start = '$startapp', cal_end = '$endapp', cal_status = '$statusapp'";
-            mysqli_query($conn, $sql);
+            //$sql = "INSERT INTO calendar SET cal_id = '$appointid', cal_name = '$hospname', cal_start = '$startapp', cal_end = '$endapp', cal_status = '$statusapp'";
+            //mysqli_query($conn, $sql);
+            $sql = $conn->prepare("INSERT INTO calendar SET cal_id = ?, cal_name =?, cal_start = ?, cal_end = ?, cal_status = ?");
+            $sql->bind_param("issss", $appointid, $hospname, $startapp, $endapp, $statusapp);
+            $sql->execute();
         }
     }
 }
@@ -99,13 +115,21 @@ foreach ($result2 as $row2) {
         $end = $appdate." ".date('H:i', strtotime('22:00:00'));
     }
 
-    $valid2 = mysqli_query($conn, "SELECT COUNT(*) as Total FROM calendar WHERE cal_id = '$reqid'");
-    $ans2 = mysqli_fetch_all($valid2, MYSQLI_ASSOC);
+    //$valid2 = mysqli_query($conn, "SELECT COUNT(*) as Total FROM calendar WHERE cal_id = '$reqid'");
+    //$ans2 = mysqli_fetch_all($valid2, MYSQLI_ASSOC);
+
+    $valid2 = $conn->prepare("SELECT COUNT(*) as Total FROM calendar WHERE cal_id =? ");
+    $valid2->bind_param("i", $reqid);
+    $valid2->execute();
+    $ans2 = $valid2->get_result()->fetch_all(MYSQLI_ASSOC);
 
     foreach ($ans2 as $key2) {
         if ($key2['Total'] < 1) {
-            $sql2 = "INSERT INTO calendar SET cal_id = '$reqid', cal_name = '$packname', cal_start = '$start', cal_end = '$end', cal_status = '$status'";
-            mysqli_query($conn, $sql2);
+            //$sql2 = "INSERT INTO calendar SET cal_id = '$reqid', cal_name = '$packname', cal_start = '$start', cal_end = '$end', cal_status = '$status'";
+            //mysqli_query($conn, $sql2);
+            $sql2 = $conn->prepare("INSERT INTO calendar SET cal_id = ?, cal_name = ?, cal_start = ?, cal_end = ?, cal_status = ?");
+            $sql2->bind_param("issss", $appointid, $hospname, $startapp, $endapp, $statusapp);
+            $sql2->execute();
         }
     }
 }
