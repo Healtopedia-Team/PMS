5ab148b2dc17ced463a320a533191dd1883a93a6
heavenluv <weipj128@gmail.com>
2021-05-28 12:03:24 +0800

    chatbox design --- first commit

--- financial-report-status.php
+++ financial-report-status.php
@@ -41,7 +41,6 @@ $res = $conn->prepare($sql);
 $res->bind_param("s", $hosp);
 $res->execute();
 $gross_revenue = $res->get_result()->fetch_all(MYSQLI_ASSOC);
-print_r($gross_revenue);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -109,9 +108,19 @@ print_r($gross_revenue);
                                                 </div>
 
                                             </div>
+                                            <?php 
+                                            $up_or_down = $month[$current_month] / $month[$previous_month];
+                                                if ($up_or_down >= 1){
+                                                    $res = '+'. $up_or_down*100 . '%'; 
+                                                    $style = "color:green;";
+                                                } else {
+                                                    $res = '-' . $up_or_down*100 . '%';
+                                                    $style = "color:red;";
+                                                }
+                                            ?>
                                             <div class="col-md-5">
-                                                <i class="bi bi-graph-up">
-                                                    <span style="margin:0; font-style: normal;">+15.97%</span>
+                                                <i class="bi bi-graph-up" style="<?php echo $style;?>">
+                                                    <span style="margin:0; font-style: normal;"><?php echo $up_or_down; ?></span>
                                                 </i>
                                             </div>
                                         </div>
