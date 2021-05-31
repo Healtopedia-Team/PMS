74d2d9c22d31234d2a8aea8491016155cb340ffb
heavenluv <weipj128@gmail.com>
2021-05-31 15:57:45 +0800

    po-list for admin

--- po-list-admin.php
+++ po-list-admin.php
@@ -20,13 +20,12 @@
                 $hosps->execute();
                 $hosp_list = $hosps->get_result()->fetch_all(MYSQLI_ASSOC);
 
-
                 ?>
 
                 <section class="section">
                     <div class="card">
-                        <div class="card-body">
-                            <form method="post" action="#" style="float:right">
+                        <div class="card-body" style="padding: 0;">
+                            <form method="post" action="hosp_filter.php">
                                 <span class="input-group-btn">
                                     <button class="btn btn-default" type="button" type="submit">Go!</button>
                                 </span>
@@ -38,26 +37,6 @@
                             </form>
                         </div>
                     </div>
-                    <?php
-                    if (isset($_GET['keywords'])) {
-                        $keywords = $_GET['keywords'];
-                        $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) 
-                            AS unique_date, COUNT(*) AS amount
-                            FROM `appointwoo`
-                            WHERE DATEDIFF(NOW(), FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 
-                            AND hosp_name = ? 
-                            AND statusapp='complete'
-                            GROUP BY unique_date
-                            ORDER BY unique_date DESC";
-                        //$result = mysqli_query($conn, $query);
-                        //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-                        $result = $conn->prepare($query);
-                        $result->bind_param("s", $keywords);
-                        $result->execute();
-                        $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
-                    }
-                    ?>
                     <div class="card">
                         <div class="card-body">
                             <table class="table table-striped" id="table1">
