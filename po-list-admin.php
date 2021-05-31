bce657c4106dea0d39282aba5552ee098573fc95
heavenluv <weipj128@gmail.com>
2021-05-31 16:14:09 +0800

    po-list for admin

--- po-list-admin.php
+++ po-list-admin.php
@@ -19,7 +19,6 @@
                 $hosps = $conn->prepare("SELECT hosp_name FROM hospital");
                 $hosps->execute();
                 $hosp_list = $hosps->get_result()->fetch_all(MYSQLI_ASSOC);
-
                 ?>
 
                 <section class="section">
@@ -37,6 +36,27 @@
                             </form>
                         </div>
                     </div>
+                    <?php
+                    if (isset($_POST['submit'])) {
+
+                        $keywords = $_POST['keywords'];
+                        $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) 
+                            AS unique_date, COUNT(*) AS amount
+                            FROM `appointwoo`
+                            WHERE DATEDIFF(NOW(), FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 
+                            AND hosp_name = ? 
+                            AND statusapp='complete'
+                            GROUP BY unique_date
+                            ORDER BY unique_date DESC";
+                        //$result = mysqli_query($conn, $query);
+                        //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+                        $result = $conn->prepare($query);
+                        $result->bind_param("s", $keywords);
+                        $result->execute();
+                        $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
+                    }
+                    ?>
                     <div class="card">
                         <div class="card-body">
                             <table class="table table-striped" id="table1">
