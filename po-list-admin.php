41fb8c1fde33b7c2a7fa8715ceef84d6f1bb57e4
heavenluv <weipj128@gmail.com>
2021-05-31 16:37:06 +0800

    chatbox design --- first commit

--- po-list-admin.php
+++ po-list-admin.php
@@ -4,39 +4,7 @@
                 $conn = mysqli_connect("localhost", "myhealtopedia", "Healit20.", "db_pms");
                 session_start();
                 $hosp = $_SESSION['hospital'];
-                if (isset($_POST['submit'])) {
-
-                    $keywords = $_POST['keywords'];
-                    $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) 
-                            AS unique_date, COUNT(*) AS amount
-                            FROM `appointwoo`
-                            WHERE DATEDIFF(NOW(), FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 
-                            AND hosp_name = ? 
-                            AND statusapp='complete'
-                            GROUP BY unique_date
-                            ORDER BY unique_date DESC";
-                    //$result = mysqli_query($conn, $query);
-                    //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-                    $result = $conn->prepare($query);
-                    $result->bind_param("s", $keywords);
-                    $result->execute();
-                    $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
-                } else{
-                    $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) AS unique_date, 
-                            COUNT(*) AS amount, hosp_name FROM `appointwoo` WHERE DATEDIFF(NOW(), 
-                            FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 AND statusapp='complete' 
-                            GROUP BY unique_date,hosp_name ORDER BY unique_date DESC";
-                    //$result = mysqli_query($conn, $query);
-                    //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-                    $result = $conn->prepare($query);
-                    //$result->bind_param("s", $hosp);
-                    $result->execute();
-                    $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
-                }
                 
-
                 $hosps = $conn->prepare("SELECT hosp_name FROM hospital");
                 $hosps->execute();
                 $hosp_list = $hosps->get_result()->fetch_all(MYSQLI_ASSOC);
@@ -57,6 +25,39 @@
                             </form>
                         </div>
                     </div>
+                    <?php
+                    if (isset($_POST['submit'])) {
+
+                        $keywords = $_POST['keywords'];
+                        $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) 
+                            AS unique_date, COUNT(*) AS amount
+                            FROM `appointwoo`
+                            WHERE DATEDIFF(NOW(), FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 
+                            AND hosp_name = ? 
+                            AND statusapp='complete'
+                            GROUP BY unique_date
+                            ORDER BY unique_date DESC";
+                        //$result = mysqli_query($conn, $query);
+                        //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+                        $result = $conn->prepare($query);
+                        $result->bind_param("s", $keywords);
+                        $result->execute();
+                        $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
+                    } else {
+                        $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) AS unique_date, 
+                            COUNT(*) AS amount, hosp_name FROM `appointwoo` WHERE DATEDIFF(NOW(), 
+                            FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 AND statusapp='complete' 
+                            GROUP BY unique_date,hosp_name ORDER BY unique_date DESC";
+                        //$result = mysqli_query($conn, $query);
+                        //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+                        $result = $conn->prepare($query);
+                        //$result->bind_param("s", $hosp);
+                        $result->execute();
+                        $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
+                    }
+                    ?>
                     <div class="card">
                         <div class="card-body">
                             <table class="table table-striped" id="table1">
