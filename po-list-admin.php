1822f9620bfefeb93b5dc8111897b97e7e4672d3
heavenluv <weipj128@gmail.com>
2021-05-31 15:09:12 +0800

    po-list for admin

--- po-list-admin.php
+++ po-list-admin.php
@@ -1,16 +1,13 @@
                 <?php
                 include 'finance-header.php';
 
-                $conn = mysqli_connect("localhost","myhealtopedia","Healit20.","db_pms");
+                $conn = mysqli_connect("localhost", "myhealtopedia", "Healit20.", "db_pms");
                 session_start();
-                //$hosp = $_SESSION['hospital'];
-                $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) 
-                            AS unique_date, COUNT(*) AS amount
-                            FROM `appointwoo`
-                            WHERE DATEDIFF(NOW(), FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 
-                            AND statusapp='complete'
-                            GROUP BY unique_date
-                            ORDER BY unique_date DESC";
+                $hosp = $_SESSION['hospital'];
+                $query = "SELECT DISTINCT (DATE(FROM_UNIXTIME(start_appoint, '%Y-%m-%d'))) AS unique_date, 
+                            COUNT(*) AS amount, hosp_name FROM `appointwoo` WHERE DATEDIFF(NOW(), 
+                            FROM_UNIXTIME(appointwoo.end_appoint, '%Y-%m-%d')) > 1 AND statusapp='complete' 
+                            GROUP BY unique_date,hosp_name ORDER BY unique_date DESC";
                 //$result = mysqli_query($conn, $query);
                 //$res = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
@@ -18,7 +15,7 @@
                 //$result->bind_param("s", $hosp);
                 $result->execute();
                 $res = $result->get_result()->fetch_all(MYSQLI_ASSOC);
-                
+
                 ?>
 
                 <section class="section">
@@ -30,13 +27,14 @@
                                         <th>No</th>
                                         <th>Date</th>
                                         <th>Total Number of Orders</th>
+                                        <th>Hospital</th>
                                         <th>Details</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php $i = 1; ?>
                                     <?php foreach ($res as $row) {
-                                        
+
                                     ?>
                                         <tr>
                                             <td>
@@ -46,7 +44,10 @@
                                                 <?php echo $row['unique_date']; ?>
                                             </td>
                                             <td>
-                                                <?php echo $row['amount']; ?> 
+                                                <?php echo $row['amount']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row['hosp_name']; ?>
                                             </td>
                                             <td>
                                                 <a href='report-po.php?cur_date=<?php echo $row['unique_date'] ?>' target='_blank'><button class="btn btn-info"><i class="bi bi-eye"></i></button></a>
@@ -59,31 +60,31 @@
                         </div>
                     </div>
                 </section>
-            </div>
-
-            <footer>
-                <div class="footer clearfix mb-0 text-muted">
-                    <div class="float-start">
-                        <p>2021 &copy; Healtopedia Digital</p>
-                    </div>
-                    <div class="float-end">
-                        <p>Powered By Atiq hehehe ;)<span class="text-danger"><i class="bi bi-heart"></i></span></p>
-                    </div>
                 </div>
-            </footer>
-        </div>
-    </div>
-    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-    <script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
-    <script>
-        // Simple Datatable
-        let table1 = document.querySelector('#table1');
-        let dataTable = new simpleDatatables.DataTable(table1);
-    </script>
+                <footer>
+                    <div class="footer clearfix mb-0 text-muted">
+                        <div class="float-start">
+                            <p>2021 &copy; Healtopedia Digital</p>
+                        </div>
+                        <div class="float-end">
+                            <p>Powered By Atiq hehehe ;)<span class="text-danger"><i class="bi bi-heart"></i></span></p>
+                        </div>
+                    </div>
+                </footer>
+                </div>
+                </div>
+                <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+                <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-    <script src="assets/js/main.js"></script>
-</body>
+                <script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
+                <script>
+                    // Simple Datatable
+                    let table1 = document.querySelector('#table1');
+                    let dataTable = new simpleDatatables.DataTable(table1);
+                </script>
 
-</html>
+                <script src="assets/js/main.js"></script>
+                </body>
+
+                </html>
