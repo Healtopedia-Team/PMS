16bce60b7bd9ff7e143d614df506c4dfd2a6f98d
heavenluv <weipj128@gmail.com>
2021-06-01 17:46:17 +0800

    financial report for admin

--- financial-report-status-admin.php
+++ financial-report-status-admin.php
@@ -170,7 +170,8 @@ $formattedMonthArray = array(
                 <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last" style="padding: 10px;">
-                            <h3>Financial Report <?php echo $formattedMonthArray[$sel_month] . " of " . $sel_year . " at " . $sel_hosp?></h3>
+                            <h3>Financial Report</h3>
+                            <h6><?php echo $formattedMonthArray[$sel_month] . " of " . $sel_year . " at " . $sel_hosp ?></h6>
                         </div>
                         <div class="col-12 col-md-6 order-md-2 order-first">
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
