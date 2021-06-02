b8fe7fc6cd1a42b4b1980b2365f4313401279fdf
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-06-02 11:09:03 +0800

    Update appointment-list-all.php

--- appointment-list-all.php
+++ appointment-list-all.php
@@ -400,16 +400,8 @@ if (isset($_POST['refund'])) {
                                                                     $yesterday = date('Y-m-d',strtotime("-1 days"));
                                                                     if ($appdate > $yesterday) { ?>
                                                                     <a href="ticket.php?orderid=<?php echo $orderid ?>&custid=<?php echo $custid ?>&prodid=<?php echo $prodid ?>&namecust=<?php echo $tickname ?>&icpass=<?php echo $icpass ?>" target="_blank"><button class="btn rounded-pill btn-primary" style="margin-top: 10px; float: left;">Get Ticket</button></a>
-                                                                    <?php } ?>
+                                                                    <?php }
 
-                                                                    <form method="POST">
-                                                                                <button type="submit" class="btn rounded-pill btn-warning" name="refund" style="margin-top: 10px; float: left;color: black">Request Refund</button>
-                                                                                <input type="text" name="refprice" value="<?php echo $totalori;?>">
-                                                                                <input type="text" name="refid" value="<?php echo $prodid;?>">
-                                                                                <input type="text" name="refname" value="<?php echo $prodname;?>">
-                                                                            </form>
-
-                                                                    <?php
                                                                     $data4 = mysqli_query($conn, "SELECT * FROM refund WHERE order_id = '$orderid' AND prod_price = '$totalori'");
                                                                     $data4 = mysqli_fetch_all($data4, MYSQLI_ASSOC);
 
@@ -422,6 +414,13 @@ if (isset($_POST['refund'])) {
                                                                         }
                                                                     }
                                                                     ?>
+
+                                                                    <form method="POST">
+                                                                                <button type="submit" class="btn rounded-pill btn-warning" name="refund" style="margin-top: 10px; float: left;color: black">Request Refund</button>
+                                                                                <input type="text" name="refprice" value="<?php echo $totalori;?>">
+                                                                                <input type="text" name="refid" value="<?php echo $prodid;?>">
+                                                                                <input type="text" name="refname" value="<?php echo $prodname;?>">
+                                                                            </form>
                                                                 </li>
                                                             </ul>
                                                         </div>
