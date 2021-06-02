6b1f4a4a3d1de728a693fc3a07550a2557838b92
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-06-02 11:36:56 +0800

    Update view-appointment.php

--- view-appointment.php
+++ view-appointment.php
@@ -408,7 +408,7 @@ if (isset($_POST['refund'])) {
                                                                     $data4 = mysqli_fetch_all($data4, MYSQLI_ASSOC);
 
                                                                     foreach ($data4 as $key4) {
-                                                                        if ($key4['order_id'] == "") {?>
+                                                                        if ($key4['order_id'] != "") {?>
                                                                             <form method="POST">
                                                                                 <button type="submit" class="btn rounded-pill btn-warning" name="refund" style="margin-top: 10px; float: left;color: black">Request Refund</button>
                                                                                 <input type="text" name="refprice" value="<?php echo $totalori;?>">
