ebcf73b5b4f0d622c6dcae0c303ef6c92549766a
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-06-04 13:34:28 +0800

    Update insertdata-appoint.php

--- insertdata-appoint.php
+++ insertdata-appoint.php
@@ -223,6 +223,7 @@
 
                     $query2 = $conn->prepare("INSERT INTO orderwoo SET firstname = ?, lastname = ?, order_id = ?, cust_id = ?, status = ?, order_date = ? ");
                     $query2->bind_param("ssiiss", $firstname, $lastname, $orderid, $custid, $status, $orderdate);
+
                     if ($query2->execute()) {
 
                         $data4 = $conn->prepare("SELECT * FROM appointwoo");
@@ -349,6 +350,133 @@
                                     $query4->bind_param("isssss", $reqid, $reqorder, $packname, $start, $end, $status);
                                     $query4->execute();
                                 }
+                            }
+                        }
+                    }
+                }else{
+                    $data4 = $conn->prepare("SELECT * FROM appointwoo");
+                    $data4->execute();
+                    $result4 = $data4->get_result()->fetch_all(MYSQLI_ASSOC);
+
+                    $data5 = $conn->prepare("SELECT * FROM requestappoint");
+                    $data5->execute();
+                    $result5 = $data5->get_result()->fetch_all(MYSQLI_ASSOC);
+
+                    foreach ($result4 as $row4) {
+                        $appointid = $row4['appoint_id'];
+                        $orderid = $row4['order_id'];
+                        $hospname = $row4['hosp_name'];
+                        $startapp = date('Y-m-d H:i',$row4['start_appoint']-28800);
+                        $endapp = date('Y-m-d H:i',$row4['end_appoint']-28800);
+                        $statusapp = $row4['statusapp'];
+
+                        //$valid = mysqli_query($conn, "SELECT COUNT(*) as Total FROM calendar WHERE cal_id = '$appointid'");
+                        //$ans = mysqli_fetch_all($valid, MYSQLI_ASSOC);
+
+                        $valid = $conn->prepare("SELECT COUNT(*) as Total FROM calendar WHERE cal_id =? ");
+                        $valid->bind_param("i", $appointid);
+                        $valid->execute();
+                        $ans = $valid->get_result()->fetch_all(MYSQLI_ASSOC);
+
+                        foreach ($ans as $key3) {
+                            if ($key3['Total'] < 1) {
+                                //$sql = "INSERT INTO calendar SET cal_id = '$appointid', cal_name = '$hospname', cal_start = '$startapp', cal_end = '$endapp', cal_status = '$statusapp'";
+                                //mysqli_query($conn, $sql);
+                                $query3 = $conn->prepare("INSERT INTO calendar SET cal_id = ?, cal_orderid = ?, cal_name =?, cal_start = ?, cal_end = ?, cal_status = ?");
+                                $query3->bind_param("isssss", $appointid, $orderid, $hospname, $startapp, $endapp, $statusapp);
+                                $query3->execute();
+                            }
+                        }
+                    }
+
+                    foreach ($result5 as $row5) {
+                        $reqid = $row5['request_id'];
+                        $reqorder = $row5['request_id'];
+                        $packname = $row5['req_packname'];
+                        $appdate = $row5['req_appdate'];
+                        $status = $row5['req_status'];
+
+                        if ($row5['req_apptime'] == "09:00AM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('09:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('10:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "10:00AM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('10:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('11:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "11:00AM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('11:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('12:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "12:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('12:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('13:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "01:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('13:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('14:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "02:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('14:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('15:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "03:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('15:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('16:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "04:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('16:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('17:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "05:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('17:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('18:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "06:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('18:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('19:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "07:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('19:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('20:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "08:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('20:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('21:00:00'));
+                        }
+                        elseif ($row2['req_apptime'] == "09:00PM")
+                        {
+                            $start = $appdate." ".date('H:i', strtotime('21:00:00'));
+                            $end = $appdate." ".date('H:i', strtotime('22:00:00'));
+                        }
+
+                        //$valid2 = mysqli_query($conn, "SELECT COUNT(*) as Total FROM calendar WHERE cal_id = '$reqid'");
+                        //$ans2 = mysqli_fetch_all($valid2, MYSQLI_ASSOC);
+
+                        $valid2 = $conn->prepare("SELECT COUNT(*) as Total FROM calendar WHERE cal_id =? ");
+                        $valid2->bind_param("i", $reqid);
+                        $valid2->execute();
+                        $ans2 = $valid2->get_result()->fetch_all(MYSQLI_ASSOC);
+
+                        foreach ($ans2 as $key4) {
+                            if ($key4['Total'] < 1) {
+                                //$sql2 = "INSERT INTO calendar SET cal_id = '$reqid', cal_name = '$packname', cal_start = '$start', cal_end = '$end', cal_status = '$status'";
+                                //mysqli_query($conn, $sql2);
+                                $query4 = $conn->prepare("INSERT INTO calendar SET cal_id = ?, cal_orderid = ?, cal_name =?, cal_start = ?, cal_end = ?, cal_status = ?");
+                                $query4->bind_param("isssss", $reqid, $reqorder, $packname, $start, $end, $status);
+                                $query4->execute();
                             }
                         }
                     }
