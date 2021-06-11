d837f5dbd97809e377918246db334506fa76a4ab
heavenluv <weipj128@gmail.com>
2021-06-11 19:05:07 +0800

    recipt pdf

--- receipt.php
+++ receipt.php
@@ -207,8 +207,13 @@ $hs = $result2->get_result()->fetch_all(MYSQLI_ASSOC);
         <?php //} 
         ?>
     </div>
+    <div>
+        <br>
+        <br>
+        <br>
+    </div>
 
-    <div class="billingpart" style="padding-top: 50px;">
+    <div class="billingpart">
         <table class="billing" cellspacing="0" border="0" style="width: 100%; margin: auto;">
             <tbody>
                 <tr>
