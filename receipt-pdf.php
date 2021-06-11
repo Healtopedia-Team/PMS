4973d022c154044fc5ac20b9666880c777cd621d
Hajar Rafie <43933592+hjrrfie@users.noreply.github.com>
2021-06-11 18:39:32 +0800

    Create receipt-pdf.php

---
+++ receipt-pdf.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'dompdf/autoload.inc.php';
+$cur_date = '';
+$cur_date = $_GET['cur_date'];
+$hosp = $_GET['hosp'];
+//error_reporting(E_ALL);
+//header("X-Accel-Buffering: no");
+
+// reference the Dompdf namespace
+ini_set("memory_limit", "1000M");
+ini_set("max_execution_time", "1000");
+
+
+use Dompdf\Dompdf;
+
+define('DOMPDF_ENABLE_PHP', true);
+ob_start();
+include_once 'receipt.php';
+$html = ob_get_contents();
+//var_dump($html);
+ob_end_clean();
+
+// instantiate and use the dompdf class
+//$dompdf = new Dompdf();
+$dompdf = new Dompdf();
+//require_once 'https://pms.healtopedia.com/po.php?cur_date='. $cur_date ;
+//print ob_get_level();
+//to put other html file
+//$html = file_get_contents('https://pms.healtopedia.com/po.php?cur_date='.$cur_date);
+$dompdf->setBasePath(realpath('report-style.css'));
+
+//$html = preg_replace('/>\s+</', '><', $html);
+
+//$dompdf->loadHtmlFile('https://pms.healtopedia.com/po.php?cur_date=' . $cur_date);
+$dompdf->loadHtml($html);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4','portrait');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF (1 = download and 0 = preview)
+$dompdf->stream("Receipt Order", array("Attachment"=>0));
