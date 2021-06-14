efe70458c455ce4131347bf83550d329f13b0885
Amirul Atiq <82084180+atiqrohansidek@users.noreply.github.com>
2021-06-14 12:20:14 +0800

    Update appointment-list-all.php

--- appointment-list-all.php
+++ appointment-list-all.php
@@ -66,11 +66,11 @@
                                 <?php echo $row['firstname']; ?> <?php echo $row['lastname']; ?>
                             </td>
                             <td>
-                                <?php echo date('d-m-Y', $row['order_date']); ?>
+                                <?php echo $row['order_date']; ?>
                             </td>
                             <td>
                                 <?php foreach ($timee as $rows) :
-                                    echo date('d-m-Y', $rows['start_appoint']);
+                                    echo date('Y-m-d', $rows['start_appoint']);
                                 endforeach; ?>
                             </td>
                             <td>
